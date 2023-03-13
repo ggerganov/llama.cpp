a9230b679bb13de1e2488d0d459d9d4aedb9af89
Niklas Korz <niklas@niklaskorz.de>
2023-03-13 10:48:39 +0100

    Nix: development shel, direnv and compatibility

---
+++ default.nix
@@ -0,0 +1,10 @@
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).defaultNix
--- flake.nix
+++ flake.nix
@@ -1,15 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
-        python = with pkgs; python310.withPackages (ps: with python310Packages; [
+        llama-python = with pkgs; python310.withPackages (ps: with python310Packages; [
           torch
           numpy
           sentencepiece
@@ -18,18 +22,25 @@
       {
         packages.default = pkgs.stdenv.mkDerivation {
           name = "llama.cpp";
-          buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.Accelerate
+          buildInputs = with pkgs; lib.optionals stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.Accelerate
           ];
           src = ./.;
           installPhase = ''
             mkdir -p $out/bin
             mv main $out/bin/llama-cpp
             mv quantize $out/bin/llama-cpp-quantize
-            echo "#!${python}/bin/python" > $out/bin/convert-pth-to-ggml
+            echo "#!${llama-python}/bin/python" > $out/bin/convert-pth-to-ggml
             cat convert-pth-to-ggml.py >> $out/bin/convert-pth-to-ggml
             chmod +x $out/bin/convert-pth-to-ggml
           '';
+        };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            llama-python
+          ] ++ lib.optionals stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.Accelerate
+          ];
         };
       }
     );
