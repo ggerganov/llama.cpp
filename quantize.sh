acf35ec45a5f535ccba44945c457fe5e149db5fc
Georgi Gerganov <ggerganov@gmail.com>
2023-03-13 18:14:22 +0200

    Fix file list on Mac OS

--- quantize.sh
+++ quantize.sh
@@ -7,7 +7,7 @@ if ! [[ "$1" =~ ^[0-9]{1,2}B$ ]]; then
     exit 1
 fi
 
-for i in "models/$1/ggml-model-f16.bin*"; do
+for i in `ls models/$1/ggml-model-f16.bin*`; do
     ./quantize "$i" "${i/f16/q4_0}" 2
     if [[ "$2" == "--remove-f16" ]]; then
         rm "$i"
