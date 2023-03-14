901c34d1b180271e6b49818adda59b4578bcbb34
Bernat Vadell <hounter.caza@gmail.com>
2023-03-14 20:46:41 +0100

    Update .devops/tools.sh
    Co-authored-by: Georgi Gerganov <ggerganov@gmail.com>

--- .devops/tools.sh
+++ .devops/tools.sh
@@ -10,10 +10,10 @@ shift
 arg2="$@"
 
 if [[ $arg1 == '--convert' || $arg1 == '-c' ]]; then
-  python3 ./convert-pth-to-ggml.py $arg2
+    python3 ./convert-pth-to-ggml.py $arg2
 elif  [[ $arg1 == '--quantize' || $arg1 == '-q' ]]; then
-  /app/quantize $arg2
+    /app/quantize $arg2
 else
-  echo "Unknown command: $arg1"
-  echo "Valid commands: --convert (-c) or --quantize (-q)"
+    echo "Unknown command: $arg1"
+    echo "Valid commands: --convert (-c) or --quantize (-q)"
 fi
