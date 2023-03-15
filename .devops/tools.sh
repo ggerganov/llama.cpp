44f74671cbe1b1d2d33159682441f175546aa351
Bernat Vadell <bvadell@feimsoft.com>
2023-03-15 17:45:33 +0100

    add docker action pipeline

--- .devops/tools.sh
+++ .devops/tools.sh
@@ -11,9 +11,14 @@ arg2="$@"
 
 if [[ $arg1 == '--convert' || $arg1 == '-c' ]]; then
     python3 ./convert-pth-to-ggml.py $arg2
-elif  [[ $arg1 == '--quantize' || $arg1 == '-q' ]]; then
+elif [[ $arg1 == '--quantize' || $arg1 == '-q' ]]; then
     /app/quantize $arg2
+elif [[ $arg1 == '--run' || $arg1 == '-r' ]]; then
+    /app/main $arg2
 else
     echo "Unknown command: $arg1"
-    echo "Valid commands: --convert (-c) or --quantize (-q)"
+    echo "Available commands: "
+    echo "  --run (-r)"
+    echo "  --convert (-c)"
+    echo "  --quantize (-q)"
 fi
