16b2c61a22f828ea77d9f084ca871c63bc5cc283
Justin Suess <justin.suess@westpoint.edu>
2023-03-15 15:39:38 -0400

    fixed color reset on exit (#149)
    * fixed color reset on exit
    
    * added sigint handler for ansi_color_reset
    
    * Update main.cpp
    
    ---------
    
    Co-authored-by: Georgi Gerganov <ggerganov@gmail.com>

--- main.cpp
+++ main.cpp
@@ -755,6 +755,7 @@ static bool is_interacting = false;
 
 #if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
 void sigint_handler(int signo) {
+    printf(ANSI_COLOR_RESET);
     if (signo == SIGINT) {
         if (!is_interacting) {
             is_interacting=true;
@@ -1051,6 +1052,10 @@ int main(int argc, char ** argv) {
     }
 
     ggml_free(model.ctx);
+
+    if (params.use_color) {
+        printf(ANSI_COLOR_RESET);
+    }
 
     return 0;
 }
