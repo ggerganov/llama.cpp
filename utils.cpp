cae6e8a0025984381e30f88e4266edc944ed0b13
Georgi Gerganov <ggerganov@gmail.com>
2023-03-21 22:45:22 +0200

    Add <iterator>

--- utils.cpp
+++ utils.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <fstream>
 #include <string>
+#include <iterator>
 
  #if defined(_MSC_VER) || defined(__MINGW32__)
  #include <malloc.h> // using malloc.h with MSC/MINGW
