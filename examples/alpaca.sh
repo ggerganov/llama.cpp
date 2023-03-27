120bda4c1880b676eb20c53032b7542362519df6
anzz1 <anzz1@live.com>
2023-03-27 17:22:15 +0300

    Update alpaca.sh

--- examples/alpaca.sh
+++ examples/alpaca.sh
@@ -7,4 +7,4 @@
 cd `dirname $0`
 cd ..
 
-./main -m ./models/ggml-alpaca-7b-q4.bin --color -f ./prompts/alpaca.txt -ins -b 256 --top_k 10000 --temp 0.2 --repeat_penalty 1 -t 7
+./instruct -m ./models/alpaca-7B-ggml/ggml-model-q4_0.bin --color -f ./prompts/alpaca.txt -c 2024 -n -1
