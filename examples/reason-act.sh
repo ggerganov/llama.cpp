c6e80140623637b282e5ce9eff01de6db34feef1
Tobias Lütke <tobi@shopify.com>
2023-03-28 23:06:33 +0200

    add example of re-act pattern

---
+++ examples/reason-act.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+cd `dirname $0`
+cd ..
+
+# get -m model parameter otherwise defer to default
+if [ "$1" == "-m" ]; then
+  MODEL="-m $1"
+fi
+
+./main $MODEL --color \
+    -f ./prompts/reason-act.txt \
+    -i --interactive-first \
+    --top_k 10000 --temp 0.2 --repeat_penalty 1 -t 7 -c 2048 \
+    -r "Question: " -r "Observation: " -n -1
