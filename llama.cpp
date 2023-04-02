255f019f880749fb93a8b2140a4f12696aaa2ebb
anzz1 <anzz1@live.com>
2023-04-02 15:35:09 +0300

    Revert "Ensure --mlock works properly with mmap() support"
    This reverts commit 6f23ba5ee235cbcb1eedd63b98422dd8d4392a78.

--- llama.cpp
+++ llama.cpp
@@ -1595,10 +1595,7 @@ struct llama_context * llama_init_from_file(
 
     if (params.use_mlock) {
         char *err;
-        if (!ggml_mlock(ctx->model.ctx,
-                        ctx->model.mm_addr,
-                        ctx->model.mm_length,
-                        &err)) {
+        if (!ggml_mlock(ctx->model.ctx, &err)) {
             fprintf(stderr, "%s\n", err);
             free(err);
             llama_free(ctx);
