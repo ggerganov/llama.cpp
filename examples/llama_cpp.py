d0a7ce9abf690fe6f6ff77d31de8d4942f840787
Mug <>
2023-04-10 17:12:25 +0200

    Make windows users happy (hopefully)

--- examples/llama_cpp.py
+++ examples/llama_cpp.py
@@ -38,7 +38,9 @@ def _load_shared_library(lib_base_name):
 
     if ("LLAMA_CPP_LIB" in os.environ):
         lib_base_name = os.environ["LLAMA_CPP_LIB"]
-        _lib_paths = [pathlib.Path(os.environ["LLAMA_CPP_LIB"]).resolve()]
+        _lib = pathlib.Path(lib_base_name)
+        _base_path = _lib.parent.resolve()
+        _lib_paths = [_lib.resolve()]
 
     # Add the library directory to the DLL search path on Windows (if needed)
     if sys.platform == "win32" and sys.version_info >= (3, 8):
