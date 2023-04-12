a1b4e48ba21642f8759bb77ad713a73001363721
wbpxre150 <wbpxre150@outlook.com>
2023-04-13 05:46:56 +0800

    fixO

--- examples/gpt4all.sh
+++ examples/gpt4all.sh
@@ -7,9 +7,8 @@
 cd `dirname $0`
 cd ..
 
-./main --color --instruct --threads 4 \
-       --model ./models/gpt4all-7B/gpt4all-lora-quantized.bin \
-       --file ./prompts/alpaca.txt \
+./main --color --instruct --threads 6 \
+       --model ./models/gpt4all.bin \
        --batch_size 8 --ctx_size 2048 \
        --repeat_last_n 64 --repeat_penalty 1.3 \
-       --n_predict 128 --temp 0.1 --top_k 40 --top_p 0.95
+       --n_predict 2048 --temp 0.1 --top_k 40 --top_p 0.95 --mlock
