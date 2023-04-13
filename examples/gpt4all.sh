fa651909bbad29ae0d5a1f1ad57e4b8026e46807
wbpxre150 <wbpxre150@outlook.com>
2023-04-14 01:24:32 +0800

    refector code into function.

--- examples/gpt4all.sh
+++ examples/gpt4all.sh
@@ -11,4 +11,4 @@ cd ..
        --model ./models/gpt4all.bin \
        --batch_size 8 --ctx_size 2048 \
        --repeat_last_n 64 --repeat_penalty 1.3 \
-       --n_predict 2048 --temp 0.1 --top_k 40 --top_p 0.95 --mlock
+       --n_predict 2048 --temp 0.1 --top_k 50 --top_p 0.99 --mlock
