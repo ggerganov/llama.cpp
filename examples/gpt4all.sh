10e73b08beab25b3c36159e2d22b082f809ab225
wbpxre150 <wbpxre150@outlook.com>
2023-04-14 12:41:33 +0800

    fix conflict

--- examples/gpt4all.sh
+++ examples/gpt4all.sh
@@ -7,8 +7,9 @@
 cd `dirname $0`
 cd ..
 
-./main --color --instruct --threads 6 \
-       --model ./models/gpt4all.bin \
-       --batch_size 8 --ctx_size 2048 \
+./main --color --instruct --threads 4 \
+       --model ./models/gpt4all-7B/gpt4all-lora-quantized.bin \
+       --file ./prompts/alpaca.txt \
+       --batch_size 8 --ctx_size 2048 -n -1 \
        --repeat_last_n 64 --repeat_penalty 1.3 \
-       --n_predict 2048 --temp 0.1 --top_k 50 --top_p 0.99 --mlock
+       --n_predict 128 --temp 0.1 --top_k 40 --top_p 0.95
