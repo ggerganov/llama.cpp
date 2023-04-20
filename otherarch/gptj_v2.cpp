49697d86d869f63209b06197a13e365b7487924f
Concedo <39025047+LostRuins@users.noreply.github.com>
2023-04-20 17:51:13 +0800

    adjusted down the buf memory allocation now that realloc seems to work

--- otherarch/gptj_v2.cpp
+++ otherarch/gptj_v2.cpp
@@ -382,7 +382,7 @@ bool gptj_eval(
     const int d_key = n_embd/n_head;
 
     //todo: there is a bug that causes the buffer to oom and I cannot figure it out, hack to increase size for now  
-    static size_t buf_size = 1600u*1024*1024;
+    static size_t buf_size = 512u*1024*1024;
     static void * buf = malloc(buf_size);
 
     if (mem_per_token > 0 && mem_per_token*N*1.4 > buf_size) {
