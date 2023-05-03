e2a937ca6abadc7e01e139db31e6db9dce16e3e9
Georgi Gerganov <ggerganov@gmail.com>
2023-05-03 18:43:23 +0300

    minor : fix trailing whitespaces

--- scripts/verify-checksum-models.py
+++ scripts/verify-checksum-models.py
@@ -6,13 +6,13 @@ def sha256sum(file):
     b  = bytearray(block_size)
     file_hash = hashlib.sha256()
     mv = memoryview(b)
-    with open(file, 'rb', buffering=0) as f: 
+    with open(file, 'rb', buffering=0) as f:
         while True:
             n = f.readinto(mv)
             if not n:
                 break
             file_hash.update(mv[:n])
-        
+
     return file_hash.hexdigest()
 
 # Define the path to the llama directory (parent folder of script directory)
