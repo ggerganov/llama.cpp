fce2e7e518a3f09b1214055648641d6a6957eebd
Concedo <39025047+LostRuins@users.noreply.github.com>
2023-05-15 14:53:13 +0800

    up version

--- koboldcpp.py
+++ koboldcpp.py
@@ -200,7 +200,7 @@ maxctx = 2048
 maxlen = 128
 modelbusy = False
 defaultport = 5001
-KcppVersion = "1.21.3"
+KcppVersion = "1.22"
 
 class ServerRequestHandler(http.server.SimpleHTTPRequestHandler):
     sys_version = ""
