dc271c52ed65e7c8dfcbaaf84dabb1f788e4f3d0
Stephan Walter <stephan@walter.name>
2023-05-17 22:12:01 +0000

    Remove unused n_parts parameter (#1509)

--- examples/common.h
+++ examples/common.h
@@ -24,7 +24,6 @@ struct gpt_params {
     int32_t seed          = -1;  // RNG seed
     int32_t n_threads     = get_num_physical_cores();
     int32_t n_predict     = -1;  // new tokens to predict
-    int32_t n_parts       = -1;  // amount of model parts (-1 = determine from model dimensions)
     int32_t n_ctx         = 512; // context size
     int32_t n_batch       = 512; // batch size for prompt processing (must be >=32 to use BLAS)
     int32_t n_keep        = 0;   // number of tokens to keep from initial prompt
