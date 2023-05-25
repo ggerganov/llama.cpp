61cd6685f419ec2318e7d8c1e1010009626c3f0a
ajasibley <125228925+ajasibley@users.noreply.github.com>
2023-05-25 02:13:01 -0700

    Fixed syntax error in update_path.sh

--- scripts/update_path.sh
+++ scripts/update_path.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Get the current shell name
-current_shell="$(basename ${builtins.getEnv "SHELL"})"
+current_shell="$(basename "$SHELL")"
 
 # Update the corresponding configuration file based on the current shell
 if [[ "$current_shell" == "bash" ]]; then
-    cat >> "${builtins.getEnv "HOME"}/.bashrc" <<EOF
-export PATH="/Users/test/.cosmo/bin:\${builtins.getEnv "PATH"}"
+    cat >> "${HOME}/.bashrc" <<EOF
+export PATH="/Users/test/.cosmo/bin:\${PATH}"
 EOF
-    source "${builtins.getEnv "HOME"}/.bashrc"
+    source "${HOME}/.bashrc"
 elif [[ "$current_shell" == "zsh" ]]; then
-    cat >> "${builtins.getEnv "HOME"}/.zshrc" <<EOF
-export PATH="/Users/test/.cosmo/bin:\${builtins.getEnv "PATH"}"
+    cat >> "${HOME}/.zshrc" <<EOF
+export PATH="/Users/test/.cosmo/bin:\${PATH}"
 EOF
-    source "${builtins.getEnv "HOME"}/.zshrc"
+    source "${HOME}/.zshrc"
 else
     echo "Unsupported shell: $current_shell"
     exit 1
