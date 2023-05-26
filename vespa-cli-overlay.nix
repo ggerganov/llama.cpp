7be1bbead5cfc0cf1ab3194c31c8e9175c120730
Aja Sibley <aja@thinkum.local>
2023-05-26 22:54:00 +0000

    Updated vespa-cli version.

--- vespa-cli-overlay.nix
+++ vespa-cli-overlay.nix
@@ -3,11 +3,11 @@ self: super:
 {
   vespa-cli = super.stdenv.mkDerivation rec {
     pname = "vespa-cli";
-    version = "8.162.29";
+    version = "8.167.17";
 
     src = super.fetchurl {
       url = "https://github.com/vespa-engine/vespa/releases/download/v${version}/vespa-cli_${version}_darwin_arm64.tar.gz";
-      sha256 = "05cdabe044ac34a74eca66ade3db49b7a1db29c691e7f8bbd1e71ab991548688";
+      sha256 = "df8ec80d0f44bc44edae81b05022def55167b4559670d9a1666d8723682e46cb";
     };
 
     buildInputs = [];
