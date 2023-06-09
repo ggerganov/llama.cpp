d28ed99e5916fb9755edf53f78beca3f02aa0050
Concedo <39025047+LostRuins@users.noreply.github.com>
2023-06-09 18:01:55 +0800

    remove unused declarations

--- model_adapter.h
+++ model_adapter.h
@@ -18,7 +18,7 @@ enum FileFormat
     BADFORMAT=0, //unknown, uninit, or failed to load
     GGML=1, // 1=(original llama ggml, alpaca, GPT4ALL, GPTJ header)
     GGHF=2, // 2=(llama ggmf)
-    GGJT=3, // 3=(llama ggjt) 
+    GGJT=3, // 3=(llama ggjt)
     GGJT_2=4, //newer llama format unshuffled
     GGJT_3=5, //using 16bit scalar
 
@@ -54,8 +54,6 @@ enum ModelLoadResult
     RETRY_LOAD = 2, //used if it's suspected that the model is an older format
 };
 
-bool llama_load_model(const load_model_inputs inputs, FileFormat file_format);
-generation_outputs llama_generate(const generation_inputs inputs, generation_outputs &output);
 ModelLoadResult gpttype_load_model(const load_model_inputs inputs, FileFormat in_file_format);
 generation_outputs gpttype_generate(const generation_inputs inputs, generation_outputs &output);
 
@@ -71,5 +69,5 @@ int ArrFindIndexOf(const std::vector<int> targetArray, const std::vector<int> se
 
 FileFormat check_file_format(const std::string & fname);
 void ContextFastForward(std::vector<int> &current_context_tokens, std::vector<int> &embd_inp,
- int &n_past, std::vector<int> &last_n_tokens, const int nctx, std::vector<int> &smartcontext, 
+ int &n_past, std::vector<int> &last_n_tokens, const int nctx, std::vector<int> &smartcontext,
  const bool useSmartContext, const bool requireFullSubset);
