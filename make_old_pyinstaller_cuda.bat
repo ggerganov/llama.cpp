9db2ec068f78db474443e7b12777c6204fffbd86
Concedo <39025047+LostRuins@users.noreply.github.com>
2023-06-13 22:29:38 +0800

    cuda build file

---
+++ make_old_pyinstaller_cuda.bat
@@ -0,0 +1,4 @@
+echo This file is only for my own usage, please do not use it. I am lazy.
+
+set PATH=d:\\MainApplications\\KoboldAIGPT\\KoboldAI-Horde-Bridge\\python;d:\\MainApplications\\KoboldAIGPT\\KoboldAI-Horde-Bridge\\python\\Scripts;%PATH%
+PyInstaller --noconfirm --onefile --clean --console --icon "./niko.ico" --add-data "./klite.embd;." --add-data "./koboldcpp.dll;." --add-data "./cublas64_11.dll;." --add-data "./cublasLt64_11.dll;." --add-data "./cudart64_110.dll;." --add-data "./msvcp140.dll;." --add-data "./vcruntime140.dll;." --add-data "./vcruntime140_1.dll;." --add-data "./rwkv_vocab.embd;." --add-data "./rwkv_world_vocab.embd;." "./koboldcpp.py" -n "koboldcpp.exe"
