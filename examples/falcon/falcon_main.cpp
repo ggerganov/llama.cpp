ggerganov/llama.cpp

commit 3d6ed185420204d2c41bce1a34a134545ef75bfb

Correction to 4a37251a - since we did not insert the bos token, do not need attempt to rescue it during context swap

examples/falcon/falcon_main.cpp
@@ -359,9 +359,6 @@ int main(int argc, char ** argv) {
             if (n_past + (int) embd.size() > n_ctx) {
                 const int n_left = n_past - params.n_keep;
 
-                // always keep the first token - BOS
-                n_past = std::max(1, params.n_keep);
-
                 // insert n_left/2 tokens at the start of embd from last_n_tokens
                 embd.insert(embd.begin(), last_n_tokens.begin() + n_ctx - n_left/2 - embd.size(), last_n_tokens.end() - embd.size());
 

commit 04bd2e408e64d83a8d9c84007707a6b7bf7b5689

Fixed segfault during context swap introduced by commit 3d6ed185

examples/falcon/falcon_main.cpp
@@ -359,6 +359,8 @@ int main(int argc, char ** argv) {
             if (n_past + (int) embd.size() > n_ctx) {
                 const int n_left = n_past - params.n_keep;
 
+                n_past = params.n_keep;
+
                 // insert n_left/2 tokens at the start of embd from last_n_tokens
                 embd.insert(embd.begin(), last_n_tokens.begin() + n_ctx - n_left/2 - embd.size(), last_n_tokens.end() - embd.size());
 