580e02e11ec409183510a658f3526605aa49e19e
Jhen <developer@jhen.me>
2023-08-17 19:40:27 +0800

    server : always regenerate asset hpp before compile

--- examples/server/deps.sh
+++ examples/server/deps.sh
@@ -5,9 +5,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PUBLIC=$DIR/public
 
-echo "download js bundle files"
-curl https://npm.reversehttp.com/@preact/signals-core,@preact/signals,htm/preact,preact,preact/hooks > $PUBLIC/index.js
-echo >> $PUBLIC/index.js # add newline
+if [ "$1" != "--no-download" ]; then
+  echo "download js bundle files"
+  curl https://npm.reversehttp.com/@preact/signals-core,@preact/signals,htm/preact,preact,preact/hooks > $PUBLIC/index.js
+  echo >> $PUBLIC/index.js # add newline
+fi
 
 FILES=$(ls $PUBLIC)
 
