1591e2e590762011b43b10a9b6e04f13f98f2aa5
Przemysław Pawełczyk <przemoc@gmail.com>
2023-08-27 10:10:25 +0200

    ggml : detect SSSE3 (#2825)
    * ggml : add ggml_cpu_has_ssse3
    
    * llama : show SSSE3 in system info

--- llama.cpp
+++ llama.cpp
@@ -6194,6 +6194,7 @@ const char * llama_print_system_info(void) {
     s += "WASM_SIMD = "   + std::to_string(ggml_cpu_has_wasm_simd())   + " | ";
     s += "BLAS = "        + std::to_string(ggml_cpu_has_blas())        + " | ";
     s += "SSE3 = "        + std::to_string(ggml_cpu_has_sse3())        + " | ";
+    s += "SSSE3 = "       + std::to_string(ggml_cpu_has_ssse3())       + " | ";
     s += "VSX = "         + std::to_string(ggml_cpu_has_vsx())         + " | ";
 
     return s.c_str();
