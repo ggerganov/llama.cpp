7ff671e149464d1a52b4f9e50a7819bc49e8fdaa
Adam Treat <treat.adam@gmail.com>
2023-09-14 09:58:28 -0400

    Only use vulkan with known quant that work.

--- llama.cpp
+++ llama.cpp
@@ -3394,7 +3394,11 @@ struct llama_context * llama_new_context_with_model(
 #undef LLAMA_METAL_CHECK_BUF
     }
 #elif defined(GGML_USE_KOMPUTE)
-    if (ggml_vk_has_device() && params.n_gpu_layers > 0) {
+
+    if (ggml_vk_has_device() && params.n_gpu_layers > 0
+        && (model->hparams.ftype == LLAMA_FTYPE_ALL_F32
+            || model->hparams.ftype == LLAMA_FTYPE_MOSTLY_F16
+            || model->hparams.ftype == LLAMA_FTYPE_MOSTLY_Q4_0)) {
         // this allocates all Vulkan resources and memory buffers
         ctx->ctx_kompute = ggml_vk_init();
 
