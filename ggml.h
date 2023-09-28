0ccfc62a96a6b59a8faa14d1b350493f4cd51ae2
Hua Jiang <allenhjiang@outlook.com>
2023-09-28 13:06:18 -0700

    ggml_tensor: update the structure comments. (#3283)
    * ggml_tensor: update the structure comments.
    
    * remove semicolon
    
    Co-authored-by: slaren <slarengh@gmail.com>
    
    * Update ggml.h
    
    ---------
    
    Co-authored-by: Cebtenzzre <cebtenzzre@gmail.com>
    Co-authored-by: slaren <slarengh@gmail.com>

--- ggml.h
+++ ggml.h
@@ -473,8 +473,8 @@ extern "C" {
         int     n_dims;
         int64_t ne[GGML_MAX_DIMS]; // number of elements
         size_t  nb[GGML_MAX_DIMS]; // stride in bytes:
-                                   // nb[0] = sizeof(type)
-                                   // nb[1] = nb[0]   * ne[0] + padding
+                                   // nb[0] = ggml_type_size(type)
+                                   // nb[1] = nb[0]   * (ne[0] / ggml_blck_size(type)) + padding
                                    // nb[i] = nb[i-1] * ne[i-1]
 
         // compute data
