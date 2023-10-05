09d83f04013f9e8551c3ff54449cf28e1ca00784
Adam Treat <treat.adam@gmail.com>
2023-10-05 10:52:04 -0400

    Delete TODO now that we have q8_0.

--- llama.cpp
+++ llama.cpp
@@ -6510,7 +6510,6 @@ struct llama_context * llama_new_context_with_model(
 #undef LLAMA_METAL_CHECK_BUF
         }
 #elif defined(GGML_USE_KOMPUTE)
-    // TODO(cebtenzzre): we need to check the type of each tensor because Q8_0 is not currently supported
     if (ggml_vk_has_device() && params.n_gpu_layers > 0
         && (model->arch == LLM_ARCH_LLAMA || model->arch == LLM_ARCH_FALCON)
         && (model->ftype == LLAMA_FTYPE_ALL_F32
