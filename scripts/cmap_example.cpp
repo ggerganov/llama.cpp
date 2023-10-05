1bb192fc2747f8693a0301cca5c36a09165ac1f9
pudepiedj <pudepiedj@gmail.com>
2023-10-05 11:45:20 +0100

    Add cmap_example.cpp

---
+++ scripts/cmap_example.cpp
@@ -0,0 +1,23 @@
+// example of a C/C++ equivalent data structure to the python dict
+// there are two: std::map automatically sorts on key; std::unordered_map doesn't
+
+#include <iostream>
+#include <map>
+
+int main() {
+    std::map<std::string, int> dict;
+
+    dict["apple"] = 5;
+    dict["banana"] = 2;
+    dict["orange"] = 7;
+
+
+    // Accessing elements in the map
+    std::cout << "Value of apple: " << dict["apple"] << std::endl;
+
+    for (const auto& pair : dict) {
+        std::cout << "Key: " << pair.first << ", Value: " << pair.second << std::endl;
+    }
+
+    return 0;
+}
