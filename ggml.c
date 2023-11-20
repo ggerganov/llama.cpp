e81dae7ee4db0bebdf362da1d372daee15cb2f29
mike dupont <mike.dupont@introspector.local>
2023-11-20 12:25:36 -0500

    adding the simple name lookup

--- ggml.c
+++ ggml.c
@@ -13645,6 +13645,105 @@ static void ggml_compute_forward_cross_entropy_loss_back(
 
 /////////////////////////////////
 
+const char * * ggml_op_name_table [] =  { 
+          "GGML_OP_NONE",
+        "GGML_OP_DUP",
+        "GGML_OP_ADD",
+        "GGML_OP_ADD1",
+        "GGML_OP_ACC",
+        "GGML_OP_SUB",
+        "GGML_OP_MUL",
+        "GGML_OP_DIV",
+        "GGML_OP_SQR",
+        "GGML_OP_SQRT",
+        "GGML_OP_LOG",
+        "GGML_OP_SUM",
+        "GGML_OP_SUM_ROWS",
+        "GGML_OP_MEAN",
+        "GGML_OP_ARGMAX",
+        "GGML_OP_REPEAT",
+        "GGML_OP_REPEAT_BACK",
+        "GGML_OP_CONCAT",
+        "GGML_OP_SILU_BACK",
+        "GGML_OP_NORM",
+        "GGML_OP_RMS_NORM",
+        "GGML_OP_RMS_NORM_BACK",
+        "GGML_OP_GROUP_NORM",
+        "GGML_OP_MUL_MAT",
+        "GGML_OP_OUT_PROD",
+        "GGML_OP_SCALE",
+        "GGML_OP_SET",
+        "GGML_OP_CPY",
+        "GGML_OP_CONT",
+        "GGML_OP_RESHAPE",
+        "GGML_OP_VIEW",
+        "GGML_OP_PERMUTE",
+        "GGML_OP_TRANSPOSE",
+        "GGML_OP_GET_ROWS",
+        "GGML_OP_GET_ROWS_BACK",
+        "GGML_OP_DIAG",
+        "GGML_OP_DIAG_MASK_INF",
+        "GGML_OP_DIAG_MASK_ZERO",
+        "GGML_OP_SOFT_MAX",
+        "GGML_OP_SOFT_MAX_BACK",
+        "GGML_OP_ROPE",
+        "GGML_OP_ROPE_BACK",
+        "GGML_OP_ALIBI",
+        "GGML_OP_CLAMP",
+        "GGML_OP_CONV_TRANSPOSE_1D",
+        "GGML_OP_IM2COL",
+        "GGML_OP_CONV_TRANSPOSE_2D",
+        "GGML_OP_POOL_1D",
+        "GGML_OP_POOL_2D",
+        "GGML_OP_UPSCALE",
+        "GGML_OP_FLASH_ATTN",
+        "GGML_OP_FLASH_FF",
+        "GGML_OP_FLASH_ATTN_BACK",
+        "GGML_OP_WIN_PART",
+        "GGML_OP_WIN_UNPART",
+        "GGML_OP_GET_REL_POS",
+        "GGML_OP_ADD_REL_POS",
+        "GGML_OP_UNARY",
+        "GGML_OP_MAP_UNARY",
+        "GGML_OP_MAP_BINARY",
+        "GGML_OP_MAP_CUSTOM1_F32",
+        "GGML_OP_MAP_CUSTOM2_F32",
+        "GGML_OP_MAP_CUSTOM3_F32",
+        "GGML_OP_MAP_CUSTOM1",
+        "GGML_OP_MAP_CUSTOM2",
+        "GGML_OP_MAP_CUSTOM3",
+        "GGML_OP_CROSS_ENTROPY_LOSS",
+        "GGML_OP_CROSS_ENTROPY_LOSS_BACK",
+        "GGML_OP_COUNT",
+    };
+
+    /* enum ggml_unary_op { */
+    /*     GGML_UNARY_OP_ABS, */
+    /*     GGML_UNARY_OP_SGN, */
+    /*     GGML_UNARY_OP_NEG, */
+    /*     GGML_UNARY_OP_STEP, */
+    /*     GGML_UNARY_OP_TANH, */
+    /*     GGML_UNARY_OP_ELU, */
+    /*     GGML_UNARY_OP_RELU, */
+    /*     GGML_UNARY_OP_GELU, */
+    /*     GGML_UNARY_OP_GELU_QUICK, */
+    /*     GGML_UNARY_OP_SILU, */
+    /*     GGML_UNARY_OP_LEAKY */
+    /* }; */
+
+    /* enum ggml_object_type { */
+    /*     GGML_OBJECT_TENSOR, */
+    /*     GGML_OBJECT_GRAPH, */
+    /*     GGML_OBJECT_WORK_BUFFER */
+    /* }; */
+
+    /* enum ggml_log_level { */
+    /*     GGML_LOG_LEVEL_ERROR = 2, */
+    /*     GGML_LOG_LEVEL_WARN = 3, */
+    /*     GGML_LOG_LEVEL_INFO = 4 */
+    /* }; */
+
+
 static void ggml_compute_forward(struct ggml_compute_params * params, struct ggml_tensor * tensor) {
     GGML_ASSERT(params);
 
@@ -13652,8 +13751,8 @@ static void ggml_compute_forward(struct ggml_compute_params * params, struct ggm
         return;
     }
     if (tensor->src[1]) {
-      fprintf(stderr, "JSON: { \"bop\":%d, \"src\":\"%s\",\"src2\":\"%s\",\"dst\":\"%s\"}\n",
-	    tensor->op,
+      fprintf(stderr, "JSON: { \"bop\":%s, \"src\":\"%s\",\"src2\":\"%s\",\"dst\":\"%s\"}\n",
+	    ggml_op_name_table[tensor->op],
 	    tensor->src[0]->name,
 	    tensor->src[1]->name,
 	    tensor->name);
