306754791f86e2f13089770ee0ecb1f8a783fc55
henk717 <henk@henk.tech>
2023-12-13 17:06:58 +0100

    Koboldcpp.sh Fix & Nocuda (#562)
    * .sh script V1
    
    * koboldcpp.sh polish
    
    * koboldcpp.sh dist generator
    
    * Include html's in dist
    
    * RWKV in Linux Dist
    
    * Lower dependency requirements
    
    * Eliminate wget dependency
    
    * More distinct binary name
    
    I know its technically amd64, but I don't want to cause confusion among nvidia users.
    
    * Use System OpenCL
    
    Unsure how this will behave in the pyinstaller build, but pocl ended up CPU only. With a bit of luck the pyinstaller uses the one from the actual system if compiled in a system without opencl, while conda now includes it for that specific system.
    
    * Add cblas dependency
    
    Missing this causes compile failures on some system's
    
    * ICD workaround
    
    Ideally we find a better solution, but conda forces ICD and needs this for the successful compile. However, pyinstaller then embeds the ICD causing it to be limited to the system it was compiled for. By temporarily removing the ICD pyinstaller can't find it and everything remains functional. Ideally we do this on a pyinstaller level, but I could not find any good options to do so yet.
    
    * Fix & Nocuda
    
    ---------
    
    Co-authored-by: root <root@DESKTOP-DQ1QRAG>

--- koboldcpp.sh
+++ koboldcpp.sh
@@ -15,7 +15,8 @@ if [[ $1 == "rebuild" ]]; then
 	echo Rebuild complete, you can now try to launch Koboldcpp.
 elif [[ $1 == "dist" ]]; then
 	bin/micromamba remove -r conda -n linux --force ocl-icd -y
-	bin/micromamba run -r conda -n linux pyinstaller --noconfirm --onefile --collect-all customtkinter --add-data='./koboldcpp_default.so:.' --add-data='./koboldcpp_cublas.so:.' --add-data='./koboldcpp_openblas.so:.' --add-data='./koboldcpp_clblast.so:.' --add-data='./koboldcpp_clblast_noavx2.so:.' --add-data='./klite.embd:.' --add-data='./kcpp_docs.embd:.' --add-data='./rwkv_vocab.embd:.' --add-data='./rwkv_world_vocab.embd:.' --clean --console koboldcpp.py -n "koboldcpp-linux-x64"
+	bin/micromamba run -r conda -n linux pyinstaller --noconfirm --onefile --collect-all customtkinter --add-data='./koboldcpp_default.so:.' --add-data='./koboldcpp_cublas.so:.' --add-data='./koboldcpp_openblas.so:.' --add-data='./koboldcpp_clblast.so:.' --add-data='./klite.embd:.' --add-data='./kcpp_docs.embd:.' --add-data='./rwkv_vocab.embd:.' --add-data='./rwkv_world_vocab.embd:.' --clean --console koboldcpp.py -n "koboldcpp-linux-x64"
+	bin/micromamba run -r conda -n linux pyinstaller --noconfirm --onefile --collect-all customtkinter --add-data='./koboldcpp_default.so:.' --add-data='./koboldcpp_openblas.so:.' --add-data='./koboldcpp_clblast.so:.' --add-data='./klite.embd:.' --add-data='./kcpp_docs.embd:.' --add-data='./rwkv_vocab.embd:.' --add-data='./rwkv_world_vocab.embd:.' --clean --console koboldcpp.py -n "koboldcpp-linux-x64-nocuda"
 	bin/micromamba install -r conda -n linux ocl-icd -c conda-forge -y
  else
 	bin/micromamba run -r conda -n linux python koboldcpp.py $*
