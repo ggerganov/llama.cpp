8a7cfaf946ebe50de7f69e7b1b86b37ffd6ef1d3
Georgi Gerganov <ggerganov@gmail.com>
2023-12-14 13:27:50 +0200

    ggml : add ggml_row_size(), deprecate ggml_type_sizef()

--- ggml.c
+++ ggml.c
@@ -2011,6 +2011,10 @@ size_t ggml_type_size(enum ggml_type type) {
     return type_traits[type].type_size;
 }
 
+size_t ggml_row_size(enum ggml_type type, int64_t ne) {
+    return (ggml_type_size(type)*ne)/ggml_blck_size(type);
+}
+
 double ggml_type_sizef(enum ggml_type type) {
     return ((double)(type_traits[type].type_size))/type_traits[type].blck_size;
 }
