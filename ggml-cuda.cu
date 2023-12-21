a82f85bb2bae90d772928931f073cae70ab218f2
bobqianic <129547291+bobqianic@users.noreply.github.com>
2023-12-21 11:45:46 +0000

    Update ggml-cuda.cu

--- ggml-cuda.cu
+++ ggml-cuda.cu
@@ -6825,7 +6825,7 @@ static void ggml_cuda_op_get_rows(
             break;
         default:
             // TODO: k-quants
-            GGML_ASSERT(false);
+            GGML_ASSERT(false && "Unsupported CUDA Quantization Mode");
             break;
     }
 }
