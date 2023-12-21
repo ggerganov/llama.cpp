61d435674f2ccd4549c6aa0e91fb0d9f619eb351
Georgi Gerganov <ggerganov@gmail.com>
2023-12-21 18:40:09 +0200

    Update ggml-cuda.cu

--- ggml-cuda.cu
+++ ggml-cuda.cu
@@ -6825,7 +6825,7 @@ static void ggml_cuda_op_get_rows(
             break;
         default:
             // TODO: k-quants
-            fprintf(stderr, "ERROR: Unsupported CUDA Data Type:%s\n", ggml_type_name(src0->type));
+            fprintf(stderr, "%s: unsupported type: %s\n", __func__, ggml_type_name(src0->type));
             GGML_ASSERT(false);
             break;
     }
