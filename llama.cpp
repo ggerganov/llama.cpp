7aa63a2d16ac563b24311a36942719fb36c3079c
Karthik Kumar Viswanathan <karthikkumar@gmail.com>
2024-01-13 15:05:38 -0800

    Move _WIN32_WINNT to CMake Option. Bump to CMake 3.14. Add _WIN32_WINNT Guard.

--- llama.cpp
+++ llama.cpp
@@ -987,6 +987,7 @@ struct llama_mmap {
         }
 
         if (prefetch > 0) {
+#if _WIN32_WINNT >= 0x602
             // PrefetchVirtualMemory is only present on Windows 8 and above, so we dynamically load it
             BOOL (WINAPI *pPrefetchVirtualMemory) (HANDLE, ULONG_PTR, PWIN32_MEMORY_RANGE_ENTRY, ULONG);
             HMODULE hKernel32 = GetModuleHandleW(L"kernel32.dll");
@@ -1004,6 +1005,9 @@ struct llama_mmap {
                             llama_format_win_err(GetLastError()).c_str());
                 }
             }
+#else
+            throw std::runtime_error("PrefetchVirtualMemory unavailable");
+#endif
         }
     }
 
