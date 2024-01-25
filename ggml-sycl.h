66e24c246823d48e6af6da7a7a877063df194d0b
Meng, Hengyu <hengyu.meng@intel.com>
2024-01-25 09:26:02 +0000

    pass void as arguments of ggml_backend_sycl_print_sycl_devices

--- ggml-sycl.h
+++ ggml-sycl.h
@@ -15,7 +15,7 @@ GGML_API bool   ggml_sycl_compute_forward(struct ggml_compute_params * params, s
 GGML_API ggml_backend_t ggml_backend_sycl_init(int device);
 GGML_API ggml_backend_buffer_type_t ggml_backend_sycl_buffer_type(int device);
 GGML_API ggml_backend_buffer_type_t ggml_backend_sycl_host_buffer_type(void);
-GGML_API void   ggml_backend_sycl_print_sycl_devices();
+GGML_API void   ggml_backend_sycl_print_sycl_devices(void);
 
 #ifdef  __cplusplus
 }
