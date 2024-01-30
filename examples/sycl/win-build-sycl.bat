68fd9f46bb0496737738c4a6346b0e3a6c620791
Zhang <jianyu.zhang@intel.com>
2024-01-30 10:50:59 +0800

    fix win build

--- examples/sycl/win-build-sycl.bat
+++ examples/sycl/win-build-sycl.bat
@@ -1,21 +1,23 @@
 
-rem  MIT license
-rem  Copyright (C) 2024 Intel Corporation
-rem  SPDX-License-Identifier: MIT
+::  MIT license
+::  Copyright (C) 2024 Intel Corporation
+::  SPDX-License-Identifier: MIT
 
 mkdir -p build
 cd build
 @call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 --force
 
-rem  for FP16
-rem  cmake .. -DLLAMA_SYCL=ON -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx -DLLAMA_SYCL_F16=ON # faster for long-prompt inference
+::  for FP16
+::  faster for long-prompt inference
+::  cmake -G "MinGW Makefiles" ..  -DLLAMA_SYCL=ON -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icx  -DCMAKE_BUILD_TYPE=Release -DLLAMA_SYCL_F16=ON 
 
-rem  for FP32
-cmake .. -DLLAMA_SYCL=ON -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icx -DCMAKE_BUILD_TYPE=Release
+::  for FP32
+cmake -G "MinGW Makefiles" ..  -DLLAMA_SYCL=ON -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icx  -DCMAKE_BUILD_TYPE=Release
 
-rem  build example/main only
-rem  make main
 
-rem  build all binary
+::  build example/main only
+::  make main
+
+::  build all binary
 make
 cd ..
