531b470546ab6d7ba126f2f202bb16b46e62a41b
Sang-Kil Park <sang.park@42dot.ai>
2024-02-03 02:08:14 +0900

    Add `TypeError` exception for BPE vocab.

--- convert.py
+++ convert.py
@@ -336,7 +336,7 @@ class BpeVocab:
         self.bpe_tokenizer = json.loads(open(str(fname_tokenizer), encoding="utf-8").read())
         try:
             self.vocab = self.bpe_tokenizer["model"]["vocab"]
-        except KeyError:
+        except (KeyError, TypeError):
             self.vocab = self.bpe_tokenizer
         added_tokens: dict[str, int]
         if fname_added_tokens is not None:
