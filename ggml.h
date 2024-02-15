dc828c4556d6d249cf473fb87ae6462b897e93a7
bmwl <brian.marshall@tolko.com>
2024-02-15 07:11:28 -0800

    Update ggml.h
    Align enum values
    
    Co-authored-by: Georgi Gerganov <ggerganov@gmail.com>

--- ggml.h
+++ ggml.h
@@ -659,13 +659,13 @@ extern "C" {
     };
 
     // numa strategies
-    enum ggml_numa_strategies {
-        GGML_NUMA_STRATEGY_DISABLED = 0,
+    enum ggml_numa_strategy {
+        GGML_NUMA_STRATEGY_DISABLED   = 0,
         GGML_NUMA_STRATEGY_DISTRIBUTE = 1,
-        GGML_NUMA_STRATEGY_ISOLATE = 2,
-        GGML_NUMA_STRATEGY_NUMACTL = 3,
-        GGML_NUMA_STRATEGY_MIRROR = 4,
-        GGML_NUMA_STRATEGY_MAX_VALUE = GGML_NUMA_STRATEGY_MIRROR,
+        GGML_NUMA_STRATEGY_ISOLATE    = 2,
+        GGML_NUMA_STRATEGY_NUMACTL    = 3,
+        GGML_NUMA_STRATEGY_MIRROR     = 4,
+        GGML_NUMA_STRATEGY_COUNT
     };
 
     // misc
