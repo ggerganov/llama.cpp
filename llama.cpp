2dd36d6ddd9d461fd0caee7923c61f6da1cfaee5
Douglas Hanley <thesecretaryofwar@gmail.com>
2024-02-27 00:59:44 -0600

    sort nfd map; reuse iterator

--- llama.cpp
+++ llama.cpp
@@ -8901,8 +8901,9 @@ struct llm_tokenizer_wpm {
         std::vector<uint32_t> codepoints = codepoints_from_utf8(text);
         std::vector<uint32_t> nfd_codepoints;
         for (uint32_t code : codepoints) {
-            if (nfd_map.find(code) != nfd_map.end()) {
-                for (uint32_t c : nfd_map[code]) {
+            auto it = nfd_map.find(code);
+            if (it != nfd_map.end()) {
+                for (uint32_t c : it->second) {
                     nfd_codepoints.push_back(c);
                 }
             } else {
