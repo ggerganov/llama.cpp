0fcd27dc12bee927b54cc3975b43db28a45dd388
Xuan Son Nguyen <thichthat@gmail.com>
2024-02-28 00:25:49 +0100

    Update examples/server/server.cpp
    Co-authored-by: Jared Van Bortel <cebtenzzre@gmail.com>

--- examples/server/server.cpp
+++ examples/server/server.cpp
@@ -2777,7 +2777,7 @@ inline void signal_handler(int signal) {
     if (is_terminating.test_and_set()) {
         // in case it hangs, we can force terminate the server by hitting Ctrl+C twice
         // this is for better developer experience, we can remove when the server is stable enough
-        fprintf(stderr, "SIGINT is received second time, exit immediately...\n");
+        fprintf(stderr, "Received second interrupt, terminating immediately.\n");
         exit(1);
     }
     shutdown_handler(signal);
