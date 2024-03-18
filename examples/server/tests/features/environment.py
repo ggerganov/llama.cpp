456048448852fdd0d87c5234ccd9fe47fb764765
Jared Van Bortel <jared@nomic.ai>
2024-03-18 14:26:02 -0400

    server tests : remove seemingly redundant newlines in print()

--- examples/server/tests/features/environment.py
+++ examples/server/tests/features/environment.py
@@ -12,8 +12,8 @@ import psutil
 def before_scenario(context, scenario):
     context.debug = 'DEBUG' in os.environ and os.environ['DEBUG'] == 'ON'
     if context.debug:
-        print("DEBUG=ON\n")
-    print(f"\x1b[33;42mStarting new scenario: {scenario.name}!\x1b[0m\n")
+        print("DEBUG=ON")
+    print(f"\x1b[33;42mStarting new scenario: {scenario.name}!\x1b[0m")
     port = 8080
     if 'PORT' in os.environ:
         port = int(os.environ['PORT'])
@@ -27,13 +27,13 @@ def after_scenario(context, scenario):
             return
         if scenario.status == "failed":
             if 'GITHUB_ACTIONS' in os.environ:
-                print(f"\x1b[33;101mSCENARIO FAILED: {scenario.name} server logs:\x1b[0m\n\n")
+                print(f"\x1b[33;101mSCENARIO FAILED: {scenario.name} server logs:\x1b[0m\n")
                 if os.path.isfile('llama.log'):
                     with closing(open('llama.log', 'r')) as f:
                         for line in f:
                             print(line)
             if not is_server_listening(context.server_fqdn, context.server_port):
-                print("\x1b[33;101mERROR: Server stopped listening\x1b[0m\n")
+                print("\x1b[33;101mERROR: Server stopped listening\x1b[0m")
 
         if not pid_exists(context.server_process.pid):
             assert False, f"Server not running pid={context.server_process.pid} ..."
@@ -52,14 +52,14 @@ def after_scenario(context, scenario):
                 server_kill_hard(context)
     except:
         exc = sys.exception()
-        print("error in after scenario: \n")
+        print("error in after scenario:")
         print(exc)
-        print("*** print_tb: \n")
+        print("*** print_tb:")
         traceback.print_tb(exc.__traceback__, file=sys.stdout)
 
 
 def server_graceful_shutdown(context):
-    print(f"shutting down server pid={context.server_process.pid} ...\n")
+    print(f"shutting down server pid={context.server_process.pid} ...")
     if os.name == 'nt':
         os.kill(context.server_process.pid, signal.CTRL_C_EVENT)
     else:
@@ -67,7 +67,7 @@ def server_graceful_shutdown(context):
 
 
 def server_kill(context):
-    print(f"killing server pid={context.server_process.pid} ...\n")
+    print(f"killing server pid={context.server_process.pid} ...")
     context.server_process.kill()
 
 
@@ -75,7 +75,7 @@ def server_kill_hard(context):
     pid = context.server_process.pid
     path = context.server_path
 
-    print(f"Server dangling exits, hard killing force {pid}={path}...\n")
+    print(f"Server dangling exits, hard killing force {pid}={path}...")
     try:
         psutil.Process(pid).kill()
     except psutil.NoSuchProcess:
@@ -88,7 +88,7 @@ def is_server_listening(server_fqdn, server_port):
         result = sock.connect_ex((server_fqdn, server_port))
         _is_server_listening = result == 0
         if _is_server_listening:
-            print(f"server is listening on {server_fqdn}:{server_port}...\n")
+            print(f"server is listening on {server_fqdn}:{server_port}...")
         return _is_server_listening
 
 
