96dce970917bf962e12de0bdb5ed86a9a9753c53
Julia Longtin <julia.longtin@gmail.com>
2024-03-23 14:28:29 +0000

    import stdint.h for sizeSt.

--- ggml-phi-knc-dot_q5_K_q8_K.c
+++ ggml-phi-knc-dot_q5_K_q8_K.c
@@ -1,3 +1,5 @@
+// For size_t
+#include <stdint.h>
 
 /* A forward declaration, to keep GCC happy. */
 void ggml_vec_dot_q5_K_q8_K(int n, float * restrict s, size_t bs, const void * restrict vx, size_t bx, const void * restrict vy,  size_t by, int nrc);
