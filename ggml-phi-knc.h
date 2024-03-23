ff29b659c892fe0eff50dacc5f38c81902690b51
Julia Longtin <julia.longtin@gmail.com>
2024-03-23 16:19:17 +0000

    formatting improvement.

--- ggml-phi-knc.h
+++ ggml-phi-knc.h
@@ -6,7 +6,8 @@
 extern "C" {
 #endif
 
-void ggml_vec_dot_f32(int n, float * restrict s, size_t bs, const float * restrict x, size_t bx, const float * restrict y, size_t by, int nrc);
+  /* A forward declaration, to keep GCC happy. */
+  void ggml_vec_dot_f32(int n, float * restrict s, size_t bs, const float * restrict x, size_t bx, const float * restrict y, size_t by, int nrc);
 
 #ifdef  __cplusplus
 }
