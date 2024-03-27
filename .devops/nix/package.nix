df639e07d742b821100f7a1091cee2a1d217d023
hutli <jensstaermose@hotmail.com>
2024-03-27 17:25:05 +0100

    only using explicit blas if hostPlatform is allowed

--- .devops/nix/package.nix
+++ .devops/nix/package.nix
@@ -200,7 +200,7 @@ effectiveStdenv.mkDerivation (
       ++ optionals useMpi [ mpi ]
       ++ optionals useOpenCL [ clblast ]
       ++ optionals useRocm rocmBuildInputs
-      ++ optionals useBlas [ blas ]
+      ++ optionals (useBlas && builtins.elem effectiveStdenv.hostPlatform.system blas.meta.platforms) [ blas ]
       ++ optionals useVulkan vulkanBuildInputs;
 
     cmakeFlags =
