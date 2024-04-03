3ff09248ffd28544b4c392a1cb30edc23a6dc21f
Julia Longtin <julia.longtin@gmail.com>
2024-04-03 19:01:18 +0000

    indent headers consistently.

--- ggml-phi-knc-dot_q5_K_q8_K.h
+++ ggml-phi-knc-dot_q5_K_q8_K.h
@@ -1,13 +1,15 @@
+// Formatted with: indent -npcs -nlp -i4 -l300
 #pragma once
 
 #include "ggml.h"
 
 #ifdef  __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 /* A forward declaration, to keep GCC happy. */
-void ggml_vec_dot_q5_K_q8_K(int n, float * restrict s, size_t bs, const void * restrict vx, size_t bx, const void * restrict vy,  size_t by, int nrc);
+    void ggml_vec_dot_q5_K_q8_K(int n, float *restrict s, size_t bs, const void *restrict vx, size_t bx, const void *restrict vy, size_t by, int nrc);
 
 #ifdef  __cplusplus
 }
