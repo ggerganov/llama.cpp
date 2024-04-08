llama: dbrx: rename tensor to actual meaning. Fix normalization in graph. Permute expert tensors to the llama.cpp layout

convert-hf-to-gguf.py
@@ -1523,17 +1523,20 @@ class DbrxModel(Model):
             n_embd = self.hparams["d_model"]
 
             # Specific behavior for experts tensors: reshape to 3D and add suffix .weight
-            exp_tensor_names = {"ffn.experts.mlp.v1": (n_embd, n_ff,   n_expert),  # LLM_TENSOR_FFN_GATE_EXPS
-                                "ffn.experts.mlp.w1": (n_embd, n_ff,   n_expert),  # LLM_TENSOR_FFN_DOWN_EXPS
-                                "ffn.experts.mlp.w2": (n_ff,   n_embd, n_expert)}  # LLM_TENSOR_FFN_UP_EXPS
+            exp_tensor_names = {"ffn.experts.mlp.v1": (2, 1, 3),  # LLM_TENSOR_FFN_GATE_EXPS(n_embd, n_ff,   n_expert)
+                                "ffn.experts.mlp.w2": (1, 2, 3),  # LLM_TENSOR_FFN_DOWN_EXPS(n_ff,   n_embd, n_expert)
+                                "ffn.experts.mlp.w1": (2, 1, 3)}  # LLM_TENSOR_FFN_UP_EXPS  (n_embd, n_ff,   n_expert)
             experts = False
             for exp_tensor_name in exp_tensor_names.keys():
                 if name.find(exp_tensor_name) != -1 and name.find(".weight") == -1:
                     experts = True
-                    expert_reshape = exp_tensor_names[exp_tensor_name][::-1]
+                    expert_permute = exp_tensor_names[exp_tensor_name][::-1]
                     break
 
             old_dtype = data_torch.dtype
+
+            if experts:
+                data_torch = data_torch.view(n_expert, n_ff, n_embd)
 
             # convert any unsupported data types to float32
             if data_torch.dtype not in (torch.float16, torch.float32):
@@ -1557,7 +1560,7 @@ class DbrxModel(Model):
 
             # Reshape experts tensors from 2D to 3D as expected by GeLU
             if experts and n_dims == 2:
-                data = data.reshape(expert_reshape)
+                data = data.transpose(expert_permute)
                 n_dims = len(data.shape)
 
             # if f32 desired, convert any float16 to float32