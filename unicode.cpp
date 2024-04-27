581c4a0239ade14e78d515baf4fc5bf400cdd882
Georgi Gerganov <ggerganov@gmail.com>
2024-04-27 18:36:00 +0300

    unicode : try fix windows

--- unicode.cpp
+++ unicode.cpp
@@ -203,8 +203,13 @@ static inline std::wstring unicode_wstring_from_utf8(const std::string & s) {
 }
 
 static inline std::string unicode_wstring_to_utf8(const std::wstring & ws) {
+#if defined(_MSC_VER)
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> converter;
+    return converter.to_bytes(ws);
+#else
     std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
     return conv.to_bytes(ws);
+#endif
 }
 
 static std::vector<std::string> unicode_byte_encoding_process(const std::vector<std::string> & bpe_words) {
