32e672c5dd627fadb3119a2ca02fef35b5b64129
HanishKVC <hanishkvc@gmail.com>
2024-04-27 21:58:11 +0530

    ChatON: Dont log final tagged message string to screen

--- common/chaton.hpp
+++ common/chaton.hpp
@@ -348,7 +348,7 @@ inline bool chaton_tmpl_apply_single_ex(
         LOG_TEELN("DBUG:%s:Mismatch between CP[%s] and SS[%s]", __func__, cpStr.c_str(), tagged.c_str());
         exit(2);
     }
-    LOGXLN("DBUG:%s:%s:%s:%s", __func__, tmpl.c_str(), role.c_str(), tagged.c_str());
+    LOGLN("DBUG:%s:%s:%s:%s", __func__, tmpl.c_str(), role.c_str(), tagged.c_str());
     types = cp.get_types();
     lens = cp.get_partslens();
     return true;
@@ -489,7 +489,7 @@ inline bool chaton_tmpl_apply_ex(
         LOG_TEELN("DBUG:%s:Mismatch between CP[%s] and SS[%s]", __func__, cpStr.c_str(), tagged.c_str());
         exit(2);
     }
-    LOGXLN("DBUG:%s:%s:%s", __func__, tmpl.c_str(), tagged.c_str());
+    LOGLN("DBUG:%s:%s:%s", __func__, tmpl.c_str(), tagged.c_str());
     LOGLN("DBUG:%s:%s:CntSys[%d]:CntUsr[%d]:CntOthers[%d]", __func__, tmpl.c_str(), cntSystem, cntUser, cntOthers);
     types = cp.get_types();
     lens = cp.get_partslens();
