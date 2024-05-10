e4ac8ae720847b674c681e3b6218fc1c67683725
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2024-05-10 14:57:26 +0200

    Update llama.h respect current numerology

--- llama.h
+++ llama.h
@@ -139,8 +139,8 @@ extern "C" {
         LLAMA_FTYPE_MOSTLY_IQ2_M         = 29, // except 1d tensors
         LLAMA_FTYPE_MOSTLY_IQ4_XS        = 30, // except 1d tensors
         LLAMA_FTYPE_MOSTLY_IQ1_M         = 31, // except 1d tensors
-        LLAMA_FTYPE_MOSTLY_IQ1_XS        = 32, // except 1d tensors
-        LLAMA_FTYPE_MOSTLY_BF16          = 33, // except 1d tensors
+        LLAMA_FTYPE_MOSTLY_BF16          = 32, // except 1d tensors
+        LLAMA_FTYPE_MOSTLY_IQ1_XS        = 33, // except 1d tensors
 
         LLAMA_FTYPE_GUESSED = 1024, // not specified in the model file
     };
