078e04d32bb8f45c80cf42f2de50c3c1d8694647
HanishKVC <hanishkvc@gmail.com>
2024-05-12 12:40:33 +0530

    ChatON:P2:meta json to hpp conversion - add k-v pairs skeleton

--- scripts/chaton-meta-json-to-hpp.py
+++ scripts/chaton-meta-json-to-hpp.py
@@ -5,9 +5,17 @@
 import sys
 import json
 
+
+def kv(j, tmpl, k1, k2, comma):
+  print("\t\t{{ \"{}\", \"{}\" }}{}".format("{}-{}".format(k1,k2), j[tmpl][k1][k2], comma))
+
+
 fp=open(sys.argv[1])
 j=json.load(fp)
 print("{")
 for tmpl in j:
-  print("\t{{ \"{}\", ".format(tmpl))
+  print("\t{{ \"{}\", {{".format(tmpl))
+  kv(j, tmpl, "global", "begin", ",")
+  kv(j, tmpl, "global", "end", ",")
+  print("\t}},")
  
