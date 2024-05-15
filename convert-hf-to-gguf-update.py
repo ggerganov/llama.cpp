4ac22a8239ea3e2f00e0adba65f52e1586681b04
Bram Vanroy <2779410+BramVanroy@users.noreply.github.com>
2024-05-15 12:55:11 +0200

    Update convert-hf-to-gguf-update.py

--- convert-hf-to-gguf-update.py
+++ convert-hf-to-gguf-update.py
@@ -77,6 +77,7 @@ models = [
     {"name": "jina-v2-en",     "tokt": TOKENIZER_TYPE.WPM, "repo": "https://huggingface.co/jinaai/jina-embeddings-v2-base-en", }, # WPM!
     {"name": "jina-v2-es",     "tokt": TOKENIZER_TYPE.BPE, "repo": "https://huggingface.co/jinaai/jina-embeddings-v2-base-es", },
     {"name": "jina-v2-de",     "tokt": TOKENIZER_TYPE.BPE, "repo": "https://huggingface.co/jinaai/jina-embeddings-v2-base-de", },
+    { "name": "phi-2",         "tokt": TOKENIZER_TYPE.BPE, "repo": "https://huggingface.co/microsoft/phi-2", },
 ]
 
 # make directory "models/tokenizers" if it doesn't exist
