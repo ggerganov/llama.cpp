f3e8fc196f44d73985cfe12d714ee052914d7b28
dm4 <sunrisedm4@gmail.com>
2024-05-15 20:01:12 +0800

    embedding : free the batch after execution (#7297)

--- examples/embedding/embedding.cpp
+++ examples/embedding/embedding.cpp
@@ -211,6 +211,7 @@ int main(int argc, char ** argv) {
 
     // clean up
     llama_print_timings(ctx);
+    llama_batch_free(batch);
     llama_free(ctx);
     llama_free_model(model);
     llama_backend_free();
