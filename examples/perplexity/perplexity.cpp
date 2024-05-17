e3f66f8f03ba6ea819aa8cf04813e284d3b83496
strawberrymelonpanda <152940198+strawberrymelonpanda@users.noreply.github.com>
2024-05-17 10:26:38 -0700

    Allow < 100 tasks
    Fix floating point error with ndot printing, allow end stats on lower task numbers if multiple-choice tasks.

--- examples/perplexity/perplexity.cpp
+++ examples/perplexity/perplexity.cpp
@@ -1425,7 +1425,7 @@ static void multiple_choice_score(llama_context * ctx, const gpt_params & params
         // Use all tasks
         tasks.resize(n_task);
         printf("%s: reading tasks", __func__);
-        int n_dot = n_task/100;
+        int n_dot = std::max((int) n_task/100, 1);
         int i = 0;
         for (auto& task : tasks) {
             ++i;
@@ -1675,7 +1675,7 @@ static void multiple_choice_score(llama_context * ctx, const gpt_params & params
 
     llama_batch_free(batch);
 
-    if (n_done < 100) return;
+    if (n_done < 100 && (params.multiple_choice_tasks != 0 && params.multiple_choice_tasks < (size_t)n_task)) return;
 
     float p = 1.f*n_correct/n_done;
     float sigma = sqrt(p*(1-p)/(n_done-1));
