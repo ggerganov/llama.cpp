89b1b3814453f9b0c17fdfe46a5f2cf423a422cd
Yazan Agha-Schrader <mountaiin@icloud.com>
2024-05-30 01:13:10 +0200

    move files, clean code

--- examples/server/themes/mount-ai/promptFormats.js
+++ examples/server/themes/mount-ai/promptFormats.js
@@ -1,331 +1,331 @@
 // extended list
 export const promptFormats = {
-"alpaca": {
-template: `{{prompt}}\n\n{{history}}\n\n{{char}}:`,
+  "alpaca": {
+  template: `{{prompt}}\n\n{{history}}\n\n{{char}}:`,
 
-historyTemplate: `### {{name}}:\n{{message}}`,
+  historyTemplate: `### {{name}}:\n{{message}}`,
 
-char: "Response",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "Response",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "Instruction",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "Instruction",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: ""
-},
+  stops: ""
+  },
 
-// ----------------------------
+  // ----------------------------
 
-"chatml": {
-template: `<|im_start|>system\n{{prompt}}<|im_end|>\n{{history}}{{char}}`,
+  "chatml": {
+  template: `<|im_start|>system\n{{prompt}}<|im_end|>\n{{history}}{{char}}`,
 
-historyTemplate: `<|im_start|>{{name}}\n{{message}}`,
+  historyTemplate: `<|im_start|>{{name}}\n{{message}}`,
 
-char: "assistant",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "assistant",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "user",
-userMsgPrefix: "",
-userMsgSuffix: "<|im_end|>\n",
+  user: "user",
+  userMsgPrefix: "",
+  userMsgSuffix: "<|im_end|>\n",
 
-stops: ""
-},
+  stops: ""
+  },
 
-// ----------------------------
+  // ----------------------------
 
-"commandr": {
-template: `<BOS_TOKEN><|START_OF_TURN_TOKEN|><|SYSTEM_TOKEN|>{{prompt}}\n<|END_OF_TURN_TOKEN|>{{history}}{{char}}`,
+  "commandr": {
+  template: `<BOS_TOKEN><|START_OF_TURN_TOKEN|><|SYSTEM_TOKEN|>{{prompt}}\n<|END_OF_TURN_TOKEN|>{{history}}{{char}}`,
 
-historyTemplate: `<|START_OF_TURN_TOKEN|><|{{name}}|> {{message}}`,
+  historyTemplate: `<|START_OF_TURN_TOKEN|><|{{name}}|> {{message}}`,
 
-char: "CHATBOT_TOKEN",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "CHATBOT_TOKEN",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "User",
-userMsgPrefix: "",
-userMsgSuffix: "<|END_OF_TURN_TOKEN|>",
+  user: "USER_TOKEN",
+  userMsgPrefix: "",
+  userMsgSuffix: "<|END_OF_TURN_TOKEN|>",
 
-stops: ""
-},
-// ref: https://docs.cohere.com/docs/prompting-command-r
+  stops: ""
+  },
+  // ref: https://docs.cohere.com/docs/prompting-command-r
 
-// ----------------------------
+  // ----------------------------
 
-"llama2": {
-template: `<s>[INST] <<SYS>>\n{{prompt}}\n<</SYS>>\n\nTest Message [/INST] Test Successfull </s>{{history}}{{char}}`,
+  "llama2": {
+  template: `<s>[INST] <<SYS>>\n{{prompt}}\n<</SYS>>\n\nTest Message [/INST] Test Successfull </s>{{history}}{{char}}`,
 
-historyTemplate: `{{name}}: {{message}}`,
+  historyTemplate: `{{name}}: {{message}}`,
 
-char: "Assistant",
-charMsgPrefix: "",
-charMsgSuffix: "</s>",
+  char: "Assistant",
+  charMsgPrefix: "",
+  charMsgSuffix: "</s>",
 
-user: "User",
-userMsgPrefix: "<s>[INST] ",
-userMsgSuffix: " [/INST]",
+  user: "User",
+  userMsgPrefix: "<s>[INST] ",
+  userMsgSuffix: " [/INST]",
 
-stops: ""
-},
-// ref: https://huggingface.co/blog/llama2#how-to-prompt-llama-2
+  stops: ""
+  },
+  // ref: https://huggingface.co/blog/llama2#how-to-prompt-llama-2
 
-// ----------------------------
+  // ----------------------------
 
-"llama3": {
-template: `<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{{prompt}}{{history}}{{char}}`,
+  "llama3": {
+  template: `<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{{prompt}}{{history}}{{char}}`,
 
-historyTemplate: `<|start_header_id|>{{name}}<|end_header_id|>\n\n{{message}}<|eot_id|>`,
+  historyTemplate: `<|start_header_id|>{{name}}<|end_header_id|>\n\n{{message}}<|eot_id|>`,
 
-char: "assistant",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "assistant",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "user",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "user",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: "<|eot_id|>"
-},
-// ref: https://llama.meta.com/docs/model-cards-and-prompt-formats/meta-llama-3/#special-tokens-used-with-meta-llama-3
+  stops: "<|eot_id|>"
+  },
+  // ref: https://llama.meta.com/docs/model-cards-and-prompt-formats/meta-llama-3/#special-tokens-used-with-meta-llama-3
 
-// ----------------------------
+  // ----------------------------
 
-"openchat": {
-template: `{{history}}{{char}}`,
+  "openchat": {
+  template: `{{history}}{{char}}`,
 
-historyTemplate: `GPT4 Correct {{name}}: {{message}}<|end_of_turn|>`,
+  historyTemplate: `GPT4 Correct {{name}}: {{message}}<|end_of_turn|>`,
 
-char: "Assistant",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "Assistant",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "User",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "User",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: ""
-},
+  stops: ""
+  },
 
-// ----------------------------
+  // ----------------------------
 
-"phi3": {
-template: `{{history}}{{char}}`,
+  "phi3": {
+  template: `{{history}}{{char}}`,
 
-historyTemplate: `<|{{name}}|>\n{{message}}<|end|>\n`,
+  historyTemplate: `<|{{name}}|>\n{{message}}<|end|>\n`,
 
-char: "assistant",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "assistant",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "user",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "user",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: "<|end|>"
-},
-// ref: https://huggingface.co/microsoft/Phi-3-mini-4k-instruct#chat-format
+  stops: "<|end|>"
+  },
+  // ref: https://huggingface.co/microsoft/Phi-3-mini-4k-instruct#chat-format
 
-// ----------------------------
+  // ----------------------------
 
-"vicuna": {
-template: `{{prompt}}\n{{history}}{{char}}`,
+  "vicuna": {
+  template: `{{prompt}}\n{{history}}{{char}}`,
 
-historyTemplate: `{{name}}: {{message}}\n`,
+  historyTemplate: `{{name}}: {{message}}\n`,
 
-char: "ASSISTANT",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "ASSISTANT",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "USER",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "USER",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: ""
-},
-// ref: https://huggingface.co/lmsys/vicuna-33b-v1.3/discussions/1
+  stops: ""
+  },
+  // ref: https://huggingface.co/lmsys/vicuna-33b-v1.3/discussions/1
 
-// ----------------------------
+  // ----------------------------
 
-"deepseekCoder": {
-template: `{{prompt}}{{history}}{{char}}:`,
+  "deepseekCoder": {
+  template: `{{prompt}}{{history}}{{char}}:`,
 
-historyTemplate: `### {{name}}:\n{{message}}`,
+  historyTemplate: `### {{name}}:\n{{message}}`,
 
-char: "Response",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "Response",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "Instruction",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "Instruction",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: "<|EOT|>"
-},
+  stops: "<|EOT|>"
+  },
 
-// ----------------------------
+  // ----------------------------
 
-"med42": {
-template: `<|system|>: {{prompt}}\n{{history}}{{char}}`,
+  "med42": {
+  template: `<|system|>: {{prompt}}\n{{history}}{{char}}`,
 
-historyTemplate: `<|{{name}}|>: {{message}}\n`,
+  historyTemplate: `<|{{name}}|>: {{message}}\n`,
 
-char: "assistant",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "assistant",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "prompter",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "prompter",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: ""
-},
+  stops: ""
+  },
 
-// ----------------------------
+  // ----------------------------
 
-"neuralchat": {
-template: `### System:\n{{prompt}}\n{{history}}{{char}}:`,
+  "neuralchat": {
+  template: `### System:\n{{prompt}}\n{{history}}{{char}}:`,
 
-historyTemplate: `### {{name}}:\n{{message}}\n`,
+  historyTemplate: `### {{name}}:\n{{message}}\n`,
 
-char: "Assistant",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "Assistant",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "User",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "User",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: ""
-},
+  stops: ""
+  },
 
-// ----------------------------
+  // ----------------------------
 
-"nousHermes": {
-template: `### Instruction: {{prompt}}\n\n{{history}}\n\n{{char}}:`,
+  "nousHermes": {
+  template: `### Instruction: {{prompt}}\n\n{{history}}\n\n{{char}}:`,
 
-historyTemplate: `### {{name}}:\n{{message}}`,
+  historyTemplate: `### {{name}}:\n{{message}}`,
 
-char: "Response",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "Response",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "Input",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "Input",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: ""
-},
+  stops: ""
+  },
 
-// ----------------------------
+  // ----------------------------
 
-"openchatMath": {
-template: `{{history}}{{char}}`,
+  "openchatMath": {
+  template: `{{history}}{{char}}`,
 
-historyTemplate: `Math Correct {{name}}: {{message}}<|end_of_turn|>`,
+  historyTemplate: `Math Correct {{name}}: {{message}}<|end_of_turn|>`,
 
-char: "Assistant",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "Assistant",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
 
-user: "User",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "User",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: ""
-},
+  stops: ""
+  },
 
-// ----------------------------
+  // ----------------------------
 
-"orion": {
-template: `<s>Human: Test Message\n\nAssistant: </s>Test Successful</s>{{history}}{{char}}:`,
+  "orion": {
+  template: `<s>Human: Test Message\n\nAssistant: </s>Test Successful</s>{{history}}{{char}}:`,
 
-historyTemplate: `{{name}}: {{message}}`,
+  historyTemplate: `{{name}}: {{message}}`,
 
-char: "Assistant </s>",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "Assistant </s>",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "Human",
-userMsgPrefix: "",
-userMsgSuffix: "\n\n",
+  user: "Human",
+  userMsgPrefix: "",
+  userMsgSuffix: "\n\n",
 
-stops: ""
-},
+  stops: ""
+  },
 
-// ----------------------------
+  // ----------------------------
 
-"sauerkraut": {
-template: `{{prompt}}\n{{history}}{{char}}`,
+  "sauerkraut": {
+  template: `{{prompt}}\n{{history}}{{char}}`,
 
-historyTemplate: `
-{{name}}: {{message}}\n`,
+  historyTemplate: `
+  {{name}}: {{message}}\n`,
 
-char: "Assistant",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "Assistant",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "User",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "User",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: ""
-},
+  stops: ""
+  },
 
-// ----------------------------
+  // ----------------------------
 
-"starlingCode": {
-template: `{{history}}{{char}}`,
+  "starlingCode": {
+  template: `{{history}}{{char}}`,
 
-historyTemplate: `Code {{name}}: {{message}}<|end_of_turn|>`,
+  historyTemplate: `Code {{name}}: {{message}}<|end_of_turn|>`,
 
-char: "Assistant",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "Assistant",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "User",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "User",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: ""
-},
+  stops: ""
+  },
 
-// ----------------------------
+  // ----------------------------
 
-"yi34b": {
-template: `{{history}} {{char}}`,
+  "yi34b": {
+  template: `{{history}} {{char}}`,
 
-historyTemplate: `{{name}}: {{message}}`,
+  historyTemplate: `{{name}}: {{message}}`,
 
-char: "Assistant",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "Assistant",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "Human",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "Human",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: ""
-},
+  stops: ""
+  },
 
-// ----------------------------
+  // ----------------------------
 
-"zephyr": {
-template: `<|system|>\n{{prompt}}</s>\n{{history}}{{char}}`,
+  "zephyr": {
+  template: `<|system|>\n{{prompt}}</s>\n{{history}}{{char}}`,
 
-historyTemplate: `<|{{name}}|>\n{{message}}</s>\n`,
+  historyTemplate: `<|{{name}}|>\n{{message}}</s>\n`,
 
-char: "assistant",
-charMsgPrefix: "",
-charMsgSuffix: "",
+  char: "assistant",
+  charMsgPrefix: "",
+  charMsgSuffix: "",
 
-user: "user",
-userMsgPrefix: "",
-userMsgSuffix: "",
+  user: "user",
+  userMsgPrefix: "",
+  userMsgSuffix: "",
 
-stops: ""
-}
-};
+  stops: ""
+  }
+  };
