84866fffd21ee829a4c70974cd2a69b774eee893
zhangkaihuo <zhangkaihuo@modelbest.cn>
2024-05-31 16:14:58 +0800

    support lm_head

--- llama.cpp
+++ llama.cpp
@@ -4941,7 +4941,8 @@ static bool llm_load_tensors(
                     // output
                     {
                         model.output_norm = ml.create_tensor(ctx_output,       tn(LLM_TENSOR_OUTPUT_NORM, "weight"), {n_embd});
-                        if (model.arch != LLM_ARCH_MINICPM){
+                        //if (model.arch != LLM_ARCH_MINICPM){
+                        {
                             model.output = ml.create_tensor(ctx_output_split, tn(LLM_TENSOR_OUTPUT, "weight"), {n_embd, n_vocab}, false);
                             // if output is NULL, init from the input tok embed
                             if (model.output == NULL) {
@@ -10120,7 +10121,7 @@ struct llm_build_context {
         cb(cur, "lmhead_scaling", -1);
 
         // lm_head
-        cur = ggml_mul_mat(ctx0, model.tok_embd, cur);
+        cur = ggml_mul_mat(ctx0, model.output, cur);
         cb(cur, "result_output", -1);
 
         ggml_build_forward_expand(gf, cur);
