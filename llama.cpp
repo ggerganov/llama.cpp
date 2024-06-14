af019105f1581d8ec66d62c92a5df2c4f57ac416
Georgi Gerganov <ggerganov@gmail.com>
2024-06-14 13:16:18 +0300

    Update llama.cpp

--- llama.cpp
+++ llama.cpp
@@ -4708,8 +4708,7 @@ static void llm_load_vocab(
             } else if (
                 tokenizer_pre == "poro-chat") {
                 vocab.type_pre = LLAMA_VOCAB_PRE_TYPE_PORO;
-            } 
-            else {
+            } else {
                 throw std::runtime_error(format("unknown pre-tokenizer type: '%s'", tokenizer_pre.c_str()));
             }
         } else {
