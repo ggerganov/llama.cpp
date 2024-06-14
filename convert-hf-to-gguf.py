cd974f14ad679cabdf70e34ccbaa22d746422e09
Elaine <elaine.zosa@gmail.com>
2024-06-14 13:09:37 +0300

    Change Poro-34B-chat to poro-chat

--- convert-hf-to-gguf.py
+++ convert-hf-to-gguf.py
@@ -477,7 +477,7 @@ class Model:
             res = "smaug-bpe"
         if chkhsh == "c7ea5862a53e4272c035c8238367063e2b270d51faa48c0f09e9d5b54746c360":
             # ref: https://huggingface.co/LumiOpen/Poro-34B-chat
-            res = "Poro-34B-chat"
+            res = "poro-chat"
         if chkhsh == "7967bfa498ade6b757b064f31e964dddbb80f8f9a4d68d4ba7998fcf281c531a":
             # ref: https://huggingface.co/jinaai/jina-embeddings-v2-base-code
             res = "jina-v2-code"
