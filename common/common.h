ce89d8a49cb69e77cb02dd38c202a74e0287fe8e
Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>
2024-06-19 21:57:36 +0200

    add --spm-infill option

--- common/common.h
+++ common/common.h
@@ -240,6 +240,8 @@ struct gpt_params {
     std::string cvector_completions_file = "examples/cvector-generator/completions.txt";
     std::string cvector_positive_file    = "examples/cvector-generator/positive.txt";
     std::string cvector_negative_file    = "examples/cvector-generator/negative.txt";
+
+    bool spm_infill = false; // suffix/prefix/middle pattern for infill
 };
 
 void gpt_params_handle_model_default(gpt_params & params);
