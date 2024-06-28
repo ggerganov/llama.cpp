d1137c20f12f5b546b3ff8ebb1bba99570083b26
Andrei Betlen <abetlen@gmail.com>
2024-06-28 15:58:02 -0400

    Add custom add_ functions

--- convert-hf-to-gguf.py
+++ convert-hf-to-gguf.py
@@ -2363,12 +2363,10 @@ class Gemma2Model(Model):
         self.gguf_writer.add_key_length(hparams["head_dim"])
         self.gguf_writer.add_value_length(hparams["head_dim"])
         self.gguf_writer.add_file_type(self.ftype)
-        self.gguf_writer.add_float32(
-            gguf.Keys.LLM.ATTN_LOGIT_SOFTCAPPING.format(arch=self.model_arch),
+        self.gguf_writer.add_attn_logit_softcapping(
             self.hparams["attn_logit_softcapping"]
         )
-        self.gguf_writer.add_float32(
-            gguf.Keys.LLM.FINAL_LOGIT_SOFTCAPPING.format(arch=self.model_arch),
+        self.gguf_writer.add_final_logit_softcapping(
             self.hparams["final_logit_softcapping"]
         )
 
