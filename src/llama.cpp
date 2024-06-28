3a2471811f7a91511b6907dd5050e2ba1de475e0
Andrei <abetlen@gmail.com>
2024-06-28 16:07:47 -0400

    Update src/llama.cpp
    Co-authored-by: slaren <slarengh@gmail.com>

--- src/llama.cpp
+++ src/llama.cpp
@@ -17402,10 +17402,11 @@ struct llama_context * llama_new_context_with_model(
         params.flash_attn = false;
     }
 
-    if (params.flash_attn && model->arch == LLM_ARCH_GEMMA2) {
-        LLAMA_LOG_WARN("%s: flash_attn is not compatible with Gemma2 - forcing off\n", __func__);
+    if (params.flash_attn && model->hparams.attn_soft_cap) {
+        LLAMA_LOG_WARN("%s: flash_attn is not compatible with attn_soft_cap - forcing off\n", __func__);
         params.flash_attn = false;
     }
+
 
     if (params.flash_attn && model->hparams.n_embd_head_k != model->hparams.n_embd_head_v) {
         LLAMA_LOG_WARN("%s: flash_attn requires n_embd_head_k == n_embd_head_v - forcing off\n", __func__);
