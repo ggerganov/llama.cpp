e8119ef67029df8105e6dd49f169930602a8c6e5
LIU Xiao <liu.xiao.88@foxmail.com>
2024-07-03 12:49:53 +0800

    PrefetchVirtualMemory is called dynamically so remove the preprocessor

--- src/llama.cpp
+++ src/llama.cpp
@@ -1801,7 +1801,6 @@ struct llama_mmap {
         }
 
         if (prefetch > 0) {
-#if _WIN32_WINNT >= 0x602
             // PrefetchVirtualMemory is only present on Windows 8 and above, so we dynamically load it
             BOOL (WINAPI *pPrefetchVirtualMemory) (HANDLE, ULONG_PTR, PWIN32_MEMORY_RANGE_ENTRY, ULONG);
             HMODULE hKernel32 = GetModuleHandleW(L"kernel32.dll");
@@ -1819,9 +1818,6 @@ struct llama_mmap {
                             llama_format_win_err(GetLastError()).c_str());
                 }
             }
-#else
-            throw std::runtime_error("PrefetchVirtualMemory unavailable");
-#endif
         }
     }
 
