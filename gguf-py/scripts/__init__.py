a083c6cf6d2ac761173287124ee051eb6acc634d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-07-19 14:53:56 -0400

    chore : Add gguf template entrypoint to scripts sub package and fix toml entry

--- gguf-py/scripts/__init__.py
+++ gguf-py/scripts/__init__.py
@@ -4,3 +4,4 @@ from .gguf_convert_endian import main as gguf_convert_endian_entrypoint
 from .gguf_dump import main as gguf_dump_entrypoint
 from .gguf_set_metadata import main as gguf_set_metadata_entrypoint
 from .gguf_new_metadata import main as gguf_new_metadata_entrypoint
+from .gguf_template import main as gguf_template_entrypoint
