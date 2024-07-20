8f9f05bf6de7177d90107fde3ea80cbabdecc6ed
jaime-m-p <>
2024-07-20 23:12:08 +0200

    Update unicode data

--- src/unicode-data.h
+++ src/unicode-data.h
@@ -13,7 +13,7 @@ struct range_nfd {
 
 static const uint32_t MAX_CODEPOINTS = 0x110000;
 
-extern const std::vector<std::pair<uint32_t, uint16_t>> unicode_ranges_flags;
+extern const std::vector<uint16_t> unicode_rle_codepoints_categs;
 extern const std::unordered_set<uint32_t> unicode_set_whitespace;
 extern const std::unordered_map<uint32_t, uint32_t> unicode_map_lowercase;
 extern const std::unordered_map<uint32_t, uint32_t> unicode_map_uppercase;
