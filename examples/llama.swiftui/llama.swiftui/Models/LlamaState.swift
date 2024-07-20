271cc20c6796011d0e9ea259eb77071d95c6729f
Georgi Gerganov <ggerganov@gmail.com>
2024-07-20 16:08:58 +0300

    minor : add space

--- examples/llama.swiftui/llama.swiftui/Models/LlamaState.swift
+++ examples/llama.swiftui/llama.swiftui/Models/LlamaState.swift
@@ -132,7 +132,7 @@ class LlamaState: ObservableObject {
         messageLog += "\(text)"
 
         Task.detached {
-            while await !llamaContext.is_done{
+            while await !llamaContext.is_done {
                 let result = await llamaContext.completion_loop()
                 await MainActor.run {
                     self.messageLog += "\(result)"
