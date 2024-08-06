7927655e42525a7bcce9dc18388b1ece3f56f587
Srihari-mcw <srihari@multicorewareinc.com>
2024-08-06 01:36:54 -0700

    Update the type name in llama.cpp

--- src/llama.cpp
+++ src/llama.cpp
@@ -4465,7 +4465,7 @@ static std::string llama_model_ftype_name(llama_ftype ftype) {
         case LLAMA_FTYPE_MOSTLY_Q5_0:     return "Q5_0";
         case LLAMA_FTYPE_MOSTLY_Q5_1:     return "Q5_1";
         case LLAMA_FTYPE_MOSTLY_Q8_0:     return "Q8_0";
-        case LLAMA_FTYPE_MOSTLY_Q8_0_B16: return "Q4_0_B16";
+        case LLAMA_FTYPE_MOSTLY_Q8_0_B16: return "Q8_0_B16";
         case LLAMA_FTYPE_MOSTLY_Q2_K:     return "Q2_K - Medium";
         case LLAMA_FTYPE_MOSTLY_Q2_K_S:   return "Q2_K - Small";
         case LLAMA_FTYPE_MOSTLY_Q3_K_S:   return "Q3_K - Small";
