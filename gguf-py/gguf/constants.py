fe957cdb9f8100e151e6bb36d0373b8239036319
Yoshi Suhara <ysuhara@nvidia.com>
2024-08-08 09:18:59 -0700

    Fix formatting issues

--- gguf-py/gguf/constants.py
+++ gguf-py/gguf/constants.py
@@ -1064,7 +1064,7 @@ MODEL_TENSORS: dict[MODEL_ARCH, list[MODEL_TENSOR]] = {
         MODEL_TENSOR.FFN_NORM,
         MODEL_TENSOR.FFN_DOWN,
         MODEL_TENSOR.FFN_UP,
-    ],   
+    ],
     # TODO
 }
 
@@ -1108,7 +1108,7 @@ MODEL_TENSOR_SKIP: dict[MODEL_ARCH, list[MODEL_TENSOR]] = {
     MODEL_ARCH.NEMOTRON: [
         MODEL_TENSOR.ROPE_FREQS,
         MODEL_TENSOR.ATTN_ROT_EMBD,
-    ],    
+    ],
 }
 
 #
