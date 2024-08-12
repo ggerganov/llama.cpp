e6e018dafe885182dc099dc5b96cb42f0ca5726f
Changyeon Kim <cyzero.kim@samsung.com>
2024-08-12 22:19:10 +0900

    Fix-up the missing initial parameter to resolve the compilation warning.
    Signed-off-by: Changyeon Kim <cyzero.kim@samsung.com>

--- ggml/src/ggml-vulkan.cpp
+++ ggml/src/ggml-vulkan.cpp
@@ -4495,7 +4495,7 @@ static void ggml_vk_acc(ggml_backend_vk_context * ctx, vk_context& subctx, const
         (uint32_t)src1->ne[0], (uint32_t)src1->ne[1], (uint32_t)src1->ne[2],(uint32_t)src1->ne[3], (uint32_t)src1->nb[0] / src1_type_size, (uint32_t)src1->nb[1] / src1_type_size, (uint32_t)src1->nb[2] / src1_type_size, (uint32_t)src1->nb[3] / src1_type_size,
         (uint32_t) dst->ne[0], (uint32_t) dst->ne[1], (uint32_t) dst->ne[2],(uint32_t) dst->ne[3], (uint32_t) dst->nb[0] /  dst_type_size, (uint32_t) dst->nb[1] /  dst_type_size, (uint32_t) dst->nb[2] /  dst_type_size, (uint32_t) dst->nb[3] /  dst_type_size,
         d_offset,
-        0.0f, 0.0f,
+        0.0f, 0.0f, 0,
     });
 }
 
