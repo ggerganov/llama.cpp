a8109e352e177870bf8aa2dde2be619e9a32fad1
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2024-08-18 18:12:04 +0400

    Update src/llama.cpp
    Co-authored-by: compilade <git@compilade.net>

--- src/llama.cpp
+++ src/llama.cpp
@@ -427,7 +427,7 @@ static const std::map<llm_kv, const char *> LLM_KV_NAMES = {
     { LLM_KV_SSM_INNER_SIZE,                "%s.ssm.inner_size"     },
     { LLM_KV_SSM_STATE_SIZE,                "%s.ssm.state_size"     },
     { LLM_KV_SSM_TIME_STEP_RANK,            "%s.ssm.time_step_rank" },
-    { LLM_KV_SSM_B_DT_RMS,                  "%s.ssm.b_dt_rms" },
+    { LLM_KV_SSM_DT_B_C_RMS,                  "%s.ssm.dt_b_c_rms" },
 
     { LLM_KV_TOKENIZER_MODEL,                "tokenizer.ggml.model"                    },
     { LLM_KV_TOKENIZER_PRE,                  "tokenizer.ggml.pre"                      },
