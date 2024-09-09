1d5a2df1ef3f0982a0d8e434b07fca9170978831
Alberto Cabrera <alberto.cabrera@codeplay.com>
2024-09-09 22:52:53 +0100

    sycl : update support condition to im2col
    Signed-off-by: Alberto Cabrera <alberto.cabrera@codeplay.com>

--- ggml/src/ggml-sycl.cpp
+++ ggml/src/ggml-sycl.cpp
@@ -5137,13 +5137,16 @@ GGML_CALL static bool ggml_backend_sycl_supports_op(ggml_backend_t backend, cons
         case GGML_OP_SCALE:
         case GGML_OP_SQR:
         case GGML_OP_CLAMP:
+            return true;
         case GGML_OP_CONT:
+            return op->src[0]->type != GGML_TYPE_BF16;
         case GGML_OP_DIAG_MASK_INF:
         case GGML_OP_SOFT_MAX:
             return true;
         case GGML_OP_ROPE:
             return ggml_is_contiguous(op->src[0]);
         case GGML_OP_IM2COL:
+            return op->src[0]->type == GGML_TYPE_F16;
         case GGML_OP_POOL_2D:
         case GGML_OP_SUM_ROWS:
         case GGML_OP_ARGSORT:
