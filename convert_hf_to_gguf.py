a74dd353dd67cd915cb7393847dd60e2db9c04e4
Molly Sophia <mollysophia379@gmail.com>
2024-10-21 16:30:27 +0800

    RWKV v6: Set EOT token to ``\n\n``
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- convert_hf_to_gguf.py
+++ convert_hf_to_gguf.py
@@ -2865,6 +2865,7 @@ class Rwkv6Model(Model):
         self.gguf_writer.add_token_types(toktypes)
         special_vocab = gguf.SpecialVocab(self.dir_model, load_merges=False)
         special_vocab.chat_template = "rwkv-world"
+        special_vocab._set_special_token("eot", 261)
         special_vocab.add_to_gguf(self.gguf_writer)
 
     def set_gguf_parameters(self):
