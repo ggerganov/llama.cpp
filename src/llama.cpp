543b1027aa75348feb242c00312ea352b8fef4fe
Molly Sophia <mollysophia379@gmail.com>
2024-10-22 19:26:41 +0800

    llama: remove useless template matching for rwkv-world
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- src/llama.cpp
+++ src/llama.cpp
@@ -21697,7 +21697,7 @@ static int32_t llama_chat_apply_template_internal(
         if (add_ass) {
             ss << "[|assistant|]";
         }
-    } else if (tmpl == "rwkv-world" || tmpl_contains("rwkv-world") || tmpl_contains("'User: ' + message['content'] + '\n\nAssistant:'")) {
+    } else if (tmpl == "rwkv-world" || tmpl_contains("rwkv-world")) {
         for (auto message : chat) {
             std::string role(message->role);
             if (role == "user") {
