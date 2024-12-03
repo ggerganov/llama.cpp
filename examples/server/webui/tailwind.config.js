70f0346f0d82d57fed7122cfe78c5d4a0ce49497
Xuan Son Nguyen <thichthat@gmail.com>
2024-12-03 19:38:44 +0100

    server : (web ui) Various improvements, now use vite as bundler (#10599)
    * hide buttons in dropdown menu
    
    * use npm as deps manager and vite as bundler
    
    * fix build
    
    * fix build (2)
    
    * fix responsive on mobile
    
    * fix more problems on mobile
    
    * sync build
    
    * (test) add CI step for verifying build
    
    * fix ci
    
    * force rebuild .hpp files
    
    * cmake: clean up generated files pre build

---
+++ examples/server/webui/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ['light', 'dark', 'cupcake', 'bumblebee', 'emerald', 'corporate', 'synthwave', 'retro', 'cyberpunk', 'valentine', 'halloween', 'garden', 'forest', 'aqua', 'lofi', 'pastel', 'fantasy', 'wireframe', 'black', 'luxury', 'dracula', 'cmyk', 'autumn', 'business', 'acid', 'lemonade', 'night', 'coffee', 'winter', 'dim', 'nord', 'sunset'],
+  }
+}
