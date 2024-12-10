71d84a5eaf9614d0f149c223c588454748e57be2
Akarshan Biswas <akarshan.biswas@gmail.com>
2024-12-10 18:47:13 +0530

    [SYCL] Remove pragma directives from mmq.cpp

--- ggml/src/ggml-sycl/mmq.cpp
+++ ggml/src/ggml-sycl/mmq.cpp
@@ -13,10 +13,7 @@
 #include "mmq.hpp"
 #include "vecdotq.hpp"
 
-// Just to make the compiler happy
-// TODO: Remove it when needed
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdivision-by-zero"
+
 
 typedef void (*allocate_tiles_sycl_t)(
     int** x_ql,
@@ -3034,4 +3031,3 @@ catch (sycl::exception const &exc) {
             << ", line:" << __LINE__ << std::endl;
   std::exit(1);
 }
-#pragma clang diagnostic pop
