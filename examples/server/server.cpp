42c44893a672a1a6a8ae69a9840ec7c1874d078a
MichelleTPY <pheyyintan@gmail.com>
2024-12-13 18:39:33 +0000

    Update server JSON response.

--- examples/server/server.cpp
+++ examples/server/server.cpp
@@ -459,7 +459,7 @@ struct server_task_result_cmpl_final : server_task_result {
     int32_t n_decoded;
     int32_t n_prompt_tokens;
     int32_t n_tokens_cached;
-    int32_t has_new_line;
+    bool has_new_line;
     std::string stopping_word;
     stop_type stop = STOP_TYPE_NONE;
 
