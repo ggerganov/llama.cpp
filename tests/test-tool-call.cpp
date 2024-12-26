f0bd69380b1d3b69ee343f01d833cbc0133a2c5f
ochafik <ochafik@google.com>
2024-12-26 21:26:25 +0000

    Update test-tool-call.cpp

--- tests/test-tool-call.cpp
+++ tests/test-tool-call.cpp
@@ -44,13 +44,10 @@ static std::unique_ptr<llama_grammar> build_grammar(const std::string & grammar_
 static bool match_string(const std::string & input, llama_grammar * grammar) {
     const auto cpts = unicode_cpts_from_utf8(input);
 
-    const llama_grammar_rules  & rules      = llama_grammar_get_rules (grammar);
-          llama_grammar_stacks & stacks_cur = llama_grammar_get_stacks(grammar);
+    auto & stacks_cur = llama_grammar_get_stacks(grammar);
 
     for (const auto & cpt : cpts) {
-        const llama_grammar_stacks stacks_prev = llama_grammar_get_stacks(grammar); // copy
-
-        llama_grammar_accept(rules, stacks_prev, cpt, stacks_cur);
+        llama_grammar_accept(grammar, cpt);
 
         if (stacks_cur.empty()) {
             // no stacks means that the grammar failed to match at this point
