9c529a7939e9d126361e7569b5b827f48aed7926
Molly Sophia <mollysophia379@gmail.com>
2025-01-03 20:06:48 +0800

    Update common/common.cpp
    Co-authored-by: Georgi Gerganov <ggerganov@gmail.com>

--- common/common.cpp
+++ common/common.cpp
@@ -889,7 +889,7 @@ struct common_init_result common_init_from_params(common_params & params) {
     }
 
     if (params.ctx_shift && !llama_kv_cache_can_shift(lctx)) {
-        LOG_WRN("%s: KV cache shifting is not supported for this model, disabling KV cache shifting'\n", __func__);
+        LOG_WRN("%s: KV cache shifting is not supported for this model, disabling KV cache shifting\n", __func__);
         params.ctx_shift = false;
     }
 
