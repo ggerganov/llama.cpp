6ddf49f5d1460fd0425808dc19c8fde11958df83
VJHack <flymyplane21@gmail.com>
2025-01-10 11:55:12 -0600

    updated path to gguf package for non-installed setups

--- gguf-py/gguf/scripts/gguf_hash.py
+++ gguf-py/gguf/scripts/gguf_hash.py
@@ -13,8 +13,8 @@ from pathlib import Path
 from tqdm import tqdm
 
 # Necessary to load the local gguf package
-if "NO_LOCAL_GGUF" not in os.environ and (Path(__file__).parent.parent.parent / 'gguf-py').exists():
-    sys.path.insert(0, str(Path(__file__).parent.parent))
+if "NO_LOCAL_GGUF" not in os.environ and (Path(__file__).parent.parent.parent.parent / 'gguf-py').exists():
+    sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
 from gguf import GGUFReader  # noqa: E402
 
