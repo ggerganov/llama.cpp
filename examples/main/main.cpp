73c4e77de89d6084de66e3f57ae1bf1749d16f74
Xuan Son Nguyen <son@huggingface.co>
2025-01-13 19:40:07 +0100

    do not activate -cnv for non-instruct models

--- examples/main/main.cpp
+++ examples/main/main.cpp
@@ -208,9 +208,13 @@ int main(int argc, char ** argv) {
 
     // auto enable conversation mode if chat template is available
     const bool has_chat_template = !common_get_builtin_chat_template(model).empty() || !params.chat_template.empty();
-    if (params.conversation_mode == COMMON_CONVERSATION_MODE_AUTO && has_chat_template) {
-        LOG_INF("%s: chat template is available, enabling conversation mode (disable it with -no-cnv)\n", __func__);
-        params.conversation_mode = COMMON_CONVERSATION_MODE_ENABLED;
+    if (params.conversation_mode == COMMON_CONVERSATION_MODE_AUTO) {
+        if (has_chat_template) {
+            LOG_INF("%s: chat template is available, enabling conversation mode (disable it with -no-cnv)\n", __func__);
+            params.conversation_mode = COMMON_CONVERSATION_MODE_ENABLED;
+        } else {
+            params.conversation_mode = COMMON_CONVERSATION_MODE_DISABLED;
+        }
     }
 
     // in case user force-activate conversation mode (via -cnv) without proper chat template, we show a warning
