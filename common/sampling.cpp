c6123e69b00012d3ed95f8d19ecf10e8115de07f
VJHack <flymyplane21@gmail.com>
2025-01-17 01:17:40 -0600

    added top-k sampler to improve performance

--- common/sampling.cpp
+++ common/sampling.cpp
@@ -168,8 +168,9 @@ struct common_sampler * common_sampler_init(const struct llama_model * model, co
 
     if (params.mirostat == 0) {
         if(params.top_n_sigma >= 0) {
-            llama_sampler_chain_add(result->chain, llama_sampler_init_temp(params.temp));
-            llama_sampler_chain_add(result->chain, llama_sampler_init_top_n_sigma(params.top_n_sigma));
+            llama_sampler_chain_add(result->chain, llama_sampler_init_top_k        (params.top_k));
+            llama_sampler_chain_add(result->chain, llama_sampler_init_temp         (params.temp));
+            llama_sampler_chain_add(result->chain, llama_sampler_init_top_n_sigma  (params.top_n_sigma));
         } else {
             for (const auto & cnstr : params.samplers) {
                 switch (cnstr) {
