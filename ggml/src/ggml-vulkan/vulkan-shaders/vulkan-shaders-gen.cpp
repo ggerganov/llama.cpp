bf3ca6bd4259cf372800f00379bf7e8b200b34d2
Rémy O <remyoudompheng@gmail.com>
2025-01-19 16:05:47 +0100

    vulkan: implement dequantize variants for coopmat2

--- ggml/src/ggml-vulkan/vulkan-shaders/vulkan-shaders-gen.cpp
+++ ggml/src/ggml-vulkan/vulkan-shaders/vulkan-shaders-gen.cpp
@@ -314,10 +314,6 @@ void matmul_shaders(bool fp16, bool matmul_id, bool coopmat, bool coopmat2, bool
     string_to_spv(shader_name + "_f16", source_name, merge_maps(base_dict, {{"DATA_A_F16", "1"}, {"B_TYPE", "float16_t"}, {"D_TYPE", "float"}}), fp16, coopmat, coopmat2, f16acc);
 
     for (const auto& tname : type_names) {
-        if (tname == "iq2_xs" && coopmat2) continue;
-        if (tname == "iq2_xxs" && coopmat2) continue;
-        if (tname == "iq3_xxs" && coopmat2) continue;
-        if (tname == "iq3_s" && coopmat2) continue;
         std::string data_a_key = "DATA_A_" + to_uppercase(tname);
         // For unaligned, load one at a time for f32/f16, or two at a time for quants
         std::string load_vec_a_unaligned = (coopmat2 || tname == "f32" || tname == "f16") ? "1" : "2";
@@ -371,9 +367,6 @@ void process_shaders() {
 
         for (const auto& tname : type_names) {
             if (tname == "f32") {
-                continue;
-            }
-            if (tname == "iq3_s" || tname == "iq3_xxs" || tname == "iq2_xs" || tname == "iq2_xxs") {
                 continue;
             }
 
