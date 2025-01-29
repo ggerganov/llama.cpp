a151674c3e073121a4058d28938c9664a4b26c3a
uvos <devnull@uvos.xyz>
2025-01-29 17:46:23 +0100

    CUDA/HIP: add warp_size to cuda_device_info

--- ggml/src/ggml-cuda/common.cuh
+++ ggml/src/ggml-cuda/common.cuh
@@ -520,6 +520,7 @@ struct ggml_cuda_device_info {
         bool    vmm;                // virtual memory support
         size_t  vmm_granularity;    // granularity of virtual memory
         size_t  total_vram;
+        int     warp_size;          // Number of threads in a dispatch
     };
 
     cuda_device_info devices[GGML_CUDA_MAX_DEVICES] = {};
