376fbba01a7f904629b8de2bcdce96e8c1c02339
Mason M <mason@bandoti.com>
2025-02-14 16:29:10 -0400

    throw exceptions in stdio transport for now

--- common/toolcall/mcp_stdio_transport.cpp
+++ common/toolcall/mcp_stdio_transport.cpp
@@ -1,17 +1,21 @@
 
 #include "mcp_stdio_transport.hpp"
 
+#include <stdexcept>
+
 toolcall::mcp_stdio_transport::mcp_stdio_transport(std::vector<std::string> argv)
     : argv_(std::move(argv))
 {
 }
 
 void toolcall::mcp_stdio_transport::start() {
+    throw std::logic_error(std::string("Function not implemented: ") + __func__);
 }
 
 void toolcall::mcp_stdio_transport::stop() {
+    throw std::logic_error(std::string("Function not implemented: ") + __func__);
 }
 
 bool toolcall::mcp_stdio_transport::send(const mcp::message_variant & /*request*/) {
-    return false;
+    throw std::logic_error(std::string("Function not implemented: ") + __func__);
 }
